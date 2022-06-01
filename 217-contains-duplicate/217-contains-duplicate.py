8684c3d64686f91f50fb9aa087c8ad8f0f3c3fdd
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-05-31 17:20:08 -0700

    Time: 646 ms (34.06%), Space: 26 MB (72.28%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,12 +1,19 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        visited = {}
-        for num in nums:
-            if num in visited:
+        visited = set()
+        for n in nums:
+            if n in visited:
                 return True
-            visited[num] = True
+            visited.add(n)
         return False
-        # O(n**2) algorithm
+        # O(n) T O(n) S algorithm 
+        # visited = {}
+        # for num in nums:
+        #     if num in visited:
+        #         return True
+        #     visited[num] = True
+        return False
+        # O(n**2) ST algorithm
         # n = len(nums)
         # for i in range(n):
         #     for j in range(i+1, n):
