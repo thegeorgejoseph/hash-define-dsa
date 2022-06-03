ef8262738d06e210cdf8156c073d398eed012fda
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-06-03 16:35:38 -0700

    Time: 29 ms (93.94%), Space: 13.9 MB (11.37%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -9,27 +9,29 @@ class Solution:
 #         iterative solutions are almost always better as long as you can do it
 #         no performance gain in this scenario because simple problem statement
 
-        if not root:
-            return None
-        
-        queue = collections.deque([root])
-        while queue:
-            current = queue.popleft()
-            current.left, current.right = current.right, current.left
-            if current.left:
-                queue.append(current.left)
-            if current.right:
-                queue.append(current.right)
-        
-        return root
-
-#         recursive solution of a tree
-#         O(n) time because every node has to be visited once, O(n) space because O(h) call stack and h == n in this scenario because everything will be in the call stack once.
 #         if not root:
 #             return None
         
-#         left = self.invertTree(root.left)
-#         right = self.invertTree(root.right)
-#         root.left, root.right = right, left
+#         queue = collections.deque([root])
+#         while queue:
+#             current = queue.popleft()
+#             current.left, current.right = current.right, current.left
+#             if current.left:
+#                 queue.append(current.left)
+#             if current.right:
+#                 queue.append(current.right)
         
 #         return root
+
+#         recursive solution of a tree
+#         O(n) time because every node has to be visited once, O(n) space because O(h) call stack and h == n in this scenario because everything will be in the call stack once.
+        if not root:
+            return None
+        
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+        root.left, root.right = right, left
+        
+        return root
+        
+        
