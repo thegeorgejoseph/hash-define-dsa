311bf26bbe736f17d0487ee5e127abcf17cd6f10
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-06-03 17:01:54 -0700

    Time: 78 ms (14.97%), Space: 15.4 MB (76.32%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -5,15 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxDepthRecurse(self,node,depth):
-        if not node:
-            return depth
-        leftMax = self.maxDepthRecurse(node.left, depth + 1)
-        rightMax = self.maxDepthRecurse(node.right, depth + 1)
-        
-        return max(leftMax, rightMax)
+#     Iterative DFS - Stack
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return self.maxDepthRecurse(root, 0)
+        
+        max_depth = 0
+        stack = collections.deque([[root,1]])
+        while stack:
+            current = stack.pop()
+            if current[0].left:
+                stack.append([current[0].left,current[1] + 1])
+            if current[0].right:
+                stack.append([current[0].right,current[1] + 1])
+                
+            max_depth = max(max_depth, current[1])
+        return max_depth
+        
+        
+        
+        
+        
+#     Recursive DFS solution is not that great because 
+#     def maxDepthRecurse(self,node,depth):
+#         if not node:
+#             return depth
+#         leftMax = self.maxDepthRecurse(node.left, depth + 1)
+#         rightMax = self.maxDepthRecurse(node.right, depth + 1)
+        
+#         return max(leftMax, rightMax)
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#         return self.maxDepthRecurse(root, 0)
         
