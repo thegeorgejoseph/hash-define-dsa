9cc2e056c79c50736a76c54577a917817e3956b6
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-06-09 18:18:40 -0700

    Time: 349 ms (36.00%), Space: 21.2 MB (55.65%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,18 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         [1,2,3,4]
+#         [24,12,8,6]
+#         prefix = [1,2,6,24]
+#         postfix = [24,24,12,4]
+#         res = [24,12,8,6]
+        
+#         res = [1,1,2,6]
+#               [24,12,8,6]
+#         most annoying question so far
+#         basically the iterative approach is easy to follow, make prefix and postfix and then
+#         just for every answer position multiply the prefix with the postfix and voila
+#         but for the "smarter" solution it is different
+#         we build the result array with the NOT the prefixes that you compute for the easy answer
+#         but the prefix thus far
+#         and then multiply with the postfixes
         n = len(nums)
         res = [1] * (len(nums))
         prefix = 1
         for i in range(n):
             res[i] = prefix
             prefix *= nums[i]
-            
+        
         postfix = 1
-        for j in range(n - 1, -1, -1):
-            res[j] *= postfix
-            postfix *= nums[j]
+        for i in range(n - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
         return res
-    
 #         #Logic approach to solving the problem
 #         #Get Prefix List and Suffix List, Handle Edge Cases
 #         #For every element the result is the multiplication of its prefix and suffix
