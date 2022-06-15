981d058ab61d476b174b28ff38e0686b187eac0a
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-06-14 18:17:00 -0700

    Time: 633 ms (51.00%), Space: 15.5 MB (73.09%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # if you choose the largest number then no. of hours needed = len(piles)
+        # if you choose the smallest number then add up each math.ceil(p / k)
+        # run Binary search between 1 and largest number and find the smallest value of k
+        left, right = 1, max(piles)
+        res = right
+        while left <= right:
+            k = left + ((right - left) // 2)
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p/k)
+            
+            if hours <= h:
+                res = min(res, k)
+                right = k - 1
+            else:
+                left = k + 1
+        return res
