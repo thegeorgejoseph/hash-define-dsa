284e73507b850a5b9fedefb7fb3236e4630b3c96
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-06-30 18:07:41 -0700

    Time: 43 ms (61.74%), Space: 13.9 MB (25.63%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        leftMin, leftMax = 0, 0
-        
+        leftMin, leftMax = 0,0
         for c in s:
             if c == "(":
                 leftMin, leftMax = leftMin + 1, leftMax + 1
@@ -9,10 +8,8 @@ class Solution:
                 leftMin, leftMax = leftMin - 1, leftMax - 1
             else:
                 leftMin, leftMax = leftMin - 1, leftMax + 1
-            if leftMax < 0:
-                return False
-            if leftMin < 0: # required because -> s = ( * ) (
-                leftMin = 0
+            if leftMax < 0: return False
+            if leftMin < 0 : leftMin = 0
         return leftMin == 0
 #         Learn this stack solution too 
 #         count = []
