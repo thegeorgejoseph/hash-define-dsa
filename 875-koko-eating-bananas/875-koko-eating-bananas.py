d113992f62da0b23581e5b152aaf4d0fd566ae34
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-07 18:06:13 -0700

    Time: 592 ms (68.23%), Space: 15.6 MB (24.15%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -4,12 +4,12 @@ class Solution:
         # if you choose the smallest number then add up each math.ceil(p / k)
         # run Binary search between 1 and largest number and find the smallest value of k
         left, right = 1, max(piles)
-        res = right
+        res = max(piles)
         while left <= right:
             k = left + ((right - left) // 2)
             hours = 0
             for p in piles:
-                hours += math.ceil(p/k)
+                hours += math.ceil(p / k)
             
             if hours <= h:
                 res = min(res, k)
