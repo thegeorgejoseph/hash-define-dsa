8b42693186d4a7c99088f6b6b11cff76a41bcd38
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-08 17:59:16 -0700

    Time: 937 ms (66.50%), Space: 72.4 MB (32.11%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,33 +1,25 @@
 class TimeMap:
 
     def __init__(self):
-        # self.store = collections.defaultdict(list)
-        self.store = {}
-        
+        self.store = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = []
-        #use defaultdict to make code smaller
-        self.store[key].append([value,timestamp])
-        
+        self.store[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
+        nums = self.store[key]
         res = ""
-        nums = self.store.get(key, []) # best way to get from a dictionary 
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            m = l + ((r - l) // 2)
-            if nums[m][1] == timestamp:
-                return nums[m][0]
-            elif nums[m][1] < timestamp:
-                res = nums[m][0]
-                l = m + 1
+        left, right = 0 , len(nums) - 1
+        while left <= right:
+            mid = left + ((right - left) // 2)
+            if timestamp == nums[mid][1]:
+                return nums[mid][0]
+            if nums[mid][1] < timestamp:
+                res = nums[mid][0]
+                left = mid + 1
             else:
-                r = m - 1
+                right = mid - 1
         return res
-        
-
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
