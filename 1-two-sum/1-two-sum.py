a414231db930f4cbdcc6318e7781f164f45fae86
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-09 14:16:06 -0700

    Time: 86 ms (72.67%), Space: 15.2 MB (50.41%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,13 +2,11 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # using enumerate probably increase the space time complexity so use it sparingly
         
-        remainder = {}
-        n = len(nums)
-        for i in range(n):
-            num = nums[i]
-            if num in remainder:
-                return [remainder[num],i]
-            remainder[target - num] = i
+        visit = {}
+        for i in range(len(nums)):
+            if target - nums[i] in visit:
+                return [i, visit[target-nums[i]]]
+            visit[nums[i]] = i
         return [-1,-1]
         
         
@@ -19,10 +17,10 @@ class Solution:
 # hashing method: if you hash the target - current element while iterating the list then when you see an element that is present in the hash set it means that the two sum exists - O(n) time space
 # sorting method: sort the elements in O(nlogn) time and two pointer approach from back and front results in a O(nlogn) time O(n) space approach which is worse than method 2
 
-        remainder = {}
-        for idx, num in enumerate(nums):
-            if num in remainder:
-                return [remainder[num],idx]
-            remainder[target - num] = idx
-        else: 
-            return [-1,-1]
+        # remainder = {}
+        # for idx, num in enumerate(nums):
+        #     if num in remainder:
+        #         return [remainder[num],idx]
+        #     remainder[target - num] = idx
+        # else: 
+        #     return [-1,-1]
