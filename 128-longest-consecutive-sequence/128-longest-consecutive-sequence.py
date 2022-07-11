a4f00049d7e6aaac65d710c6aa6e41886ed1d878
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-11 16:38:24 -0700

    Time: 532 ms (63.60%), Space: 32.3 MB (12.97%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -2,32 +2,60 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if not nums:
             return 0
+        res = 1
         cache = {}
-        max_seq = 1
-        for num in nums:
-            cache[num] = cache.get(num,False)
-        
+        for n in nums:
+            cache[n] = cache.get(n, False) 
+            
         for num in nums:
             if cache[num]:
                 continue
             seq = 1
-            cache[num] = True
             left = num - 1
             right = num + 1
-            while seq < len(nums) and left in cache:
+            cache[num] = True
+            while left in cache and seq < len(nums):
                 if not cache[left]:
                     seq += 1
                     cache[left] = True
                     left -= 1
                 else:
                     break
-            while seq < len(nums) and right in cache:
+            
+            while right in cache and seq < len(nums):
                 if not cache[right]:
                     seq += 1
                     cache[right] = True
                     right += 1
                 else:
                     break
-            max_seq = max(max_seq, seq)
-        return max_seq
+            res= max(res, seq)
+        return res
                 
+            
+        # if not nums:
+        #     return 0
+        # original = set(nums)
+        # visit = set()
+        # i = 0
+        # n = len(nums)
+        # res = 0
+        # while i < n:
+        #     curr = nums[i]
+        #     if curr in visit:
+        #         continue
+        #     visit.add(curr)
+        #     leftCount = 0
+        #     k = curr
+        #     while k - 1 in original and k - 1 not in visit:
+        #         visit.add(k)
+        #         leftCount += 1
+        #         k -= 1
+        #     rightCount = 0
+        #     k = curr
+        #     while k + 1 in original and k + 1 not in visit:
+        #         visit.add(k)
+        #         rightCount += 1
+        #         k += 1
+        #     res = max(res, leftCount + rightCount + 1)
+        # return res
