c0e110d82bfb071ea7400fb47a88bab45e15f02d
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-12 16:25:16 -0700

    Time: 72 ms (89.99%), Space: 13.9 MB (86.51%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -32,27 +32,3 @@ class Solution:
         
         return dummy.next
         
-    
-        # carry = 0
-        # dummy = ListNode(0, None)
-        # first, second = l1, l2
-        # prev = dummy
-        # while first or second:
-        #     newVal = carry
-        #     if first:
-        #         newVal += first.val
-        #         first = first.next
-        #     if second:
-        #         newVal += second.val
-        #         second = second.next
-        #     if newVal < 10:
-        #         carry = 0
-        #     else:
-        #         carry = newVal // 10
-        #         newVal = newVal % 10
-        #     prev.next = ListNode(newVal, None)
-        #     nextNode = prev.next
-        #     prev = prev.next
-        # if carry > 0:
-        #     prev.next = ListNode(carry, None)
-        # return dummy.next
