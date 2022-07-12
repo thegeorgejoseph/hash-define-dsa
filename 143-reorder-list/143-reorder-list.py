1c2c571f334109a8f9b90c00a2e63b154ba2edcc
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-12 15:08:41 -0700

    Time: 112 ms (72.70%), Space: 24.3 MB (14.19%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,23 +8,19 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow, fast = head, head.next
-        while fast and fast.next: #we check for fast.next so that we can keep track of the centre accurately
+        slow, fast = head, head.next 
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
-        
-        current = slow.next
-        slow.next = None # this is to turn the first halfs last link off
-        prev = None
-        while current:
-            nextNode = current.next
-            current.next = prev
-            prev = current
-            current = nextNode
-        
-        #prev will hold the second head now
-        #head will hold the original head
+        curr = slow.next
+        slow.next = None
+        prev = None  
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
         
         first, second = head, prev
         while first and second:
@@ -33,3 +29,30 @@ class Solution:
             second.next = temp1
             first = temp1
             second = temp2
+        
+            
+#         slow, fast = head, head.next
+#         while fast and fast.next: #we check for fast.next so that we can keep track of the centre accurately
+#             slow = slow.next
+#             fast = fast.next.next
+        
+        
+#         current = slow.next
+#         slow.next = None # this is to turn the first halfs last link off
+#         prev = None
+#         while current:
+#             nextNode = current.next
+#             current.next = prev
+#             prev = current
+#             current = nextNode
+        
+#         #prev will hold the second head now
+#         #head will hold the original head
+        
+#         first, second = head, prev
+#         while first and second:
+#             temp1, temp2 = first.next, second.next
+#             first.next = second
+#             second.next = temp1
+#             first = temp1
+#             second = temp2
