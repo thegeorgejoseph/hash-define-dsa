4d558db3bf866e1ad71e9143d621850689cce0a7
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-12 16:25:01 -0700

    Time: 81 ms (78.05%), Space: 13.9 MB (86.51%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -6,25 +6,53 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         carry = 0
-        dummy = ListNode(0, None)
         first, second = l1, l2
-        prev = dummy
+        dummy = ListNode()
+        curr = dummy
         while first or second:
-            newVal = carry
+            res = carry
             if first:
-                newVal += first.val
+                res += first.val
                 first = first.next
             if second:
-                newVal += second.val
+                res += second.val
                 second = second.next
-            if newVal < 10:
+                
+            if res < 10:
                 carry = 0
             else:
-                carry = newVal // 10
-                newVal = newVal % 10
-            prev.next = ListNode(newVal, None)
-            nextNode = prev.next
-            prev = prev.next
+                carry = res // 10
+                res = res % 10
+            
+            curr.next = ListNode(res)
+            curr = curr.next
+        
         if carry > 0:
-            prev.next = ListNode(carry, None)
+            curr.next = ListNode(carry)
+        
         return dummy.next
+        
+    
+        # carry = 0
+        # dummy = ListNode(0, None)
+        # first, second = l1, l2
+        # prev = dummy
+        # while first or second:
+        #     newVal = carry
+        #     if first:
+        #         newVal += first.val
+        #         first = first.next
+        #     if second:
+        #         newVal += second.val
+        #         second = second.next
+        #     if newVal < 10:
+        #         carry = 0
+        #     else:
+        #         carry = newVal // 10
+        #         newVal = newVal % 10
+        #     prev.next = ListNode(newVal, None)
+        #     nextNode = prev.next
+        #     prev = prev.next
+        # if carry > 0:
+        #     prev.next = ListNode(carry, None)
+        # return dummy.next
