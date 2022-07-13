3efcfff3bef79124cd4b2427e874130e1c09d11e
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-12 17:59:32 -0700

    Time: 1744 ms (16.23%), Space: 76.6 MB (23.79%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,47 +1,45 @@
 class Node:
     def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.prev, self.next = None, None
+        self.key, self.val = key, value
+        self.prev = self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.cache = {} # stores the keys to the actual nodes
-        self.left, self. right = Node(0,0), Node(0,0)
+        self.cache = {}
+        self.least, self.most = Node(0,0), Node(0,0)
+        self.least.next, self.most.prev = self.most, self.least
         
-        self.left.next, self.right.prev = self.right, self.left 
-    
     def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-    
+        PREV, NEXT = node.prev, node.next
+        PREV.next, NEXT.prev = NEXT, PREV
+        
     def insert(self, node):
-        prev = self.right.prev
+        prev = self.most.prev 
         prev.next = node
         node.prev = prev
-        node.next = self.right
-        self.right.prev = node
+        node.next = self.most
+        self.most.prev = node
         
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            return self.cache[key].value
+            return self.cache[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
+        
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
         
         if len(self.cache) > self.cap:
-            lru = self.left.next
+            lru = self.least.next
             self.remove(lru)
             del self.cache[lru.key]
-        
             
 
 
