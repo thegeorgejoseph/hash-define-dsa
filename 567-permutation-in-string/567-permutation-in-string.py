00dfca8201d2bc79dc4c23af3449c73963ee899c
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-14 16:07:28 -0700

    Time: 70 ms (94.23%), Space: 13.9 MB (68.06%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,33 +1,35 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        #Very difficult. did not understand
         if len(s1) > len(s2): return False
         
-        s1Count, s2Count = [0] * 26, [0] * 26
+        count1, count2 = [0] * 26, [0] * 26
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
+            count1[ord(s1[i]) - ord('a')] += 1
+            #the first s1 characters of s2 have been mapped too so start loop from s1 to s2
+            count2[ord(s2[i]) - ord('a')] += 1
         
         matches = 0
         for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)
+            matches += (1 if count1[i]==count2[i] else 0)
         
-        l = 0
-        for r in range(len(s1), len(s2)):
+        left = 0
+        for right in range(len(s1), len(s2)):
             if matches == 26: return True
             
-            index = ord(s2[r]) - ord('a')
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
+            index = ord(s2[right]) - ord('a')
+            count2[index] += 1
+            if count2[index] == count1[index]:
                 matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
+                # well technically the count2[index] could have kept on increasing so we need to see if its a 0 and 1 match herer
+            elif count1[index] + 1 == count2[index]:
                 matches -= 1
             
-            index = ord(s2[l]) - ord('a')
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
+            index = ord(s2[left]) - ord('a')
+            count2[index] -= 1
+            if count1[index] == count2[index]:
                 matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
+            elif count1[index] - 1 == count2[index]:
                 matches -= 1
-            l += 1
+            left += 1
         return matches == 26
+            
