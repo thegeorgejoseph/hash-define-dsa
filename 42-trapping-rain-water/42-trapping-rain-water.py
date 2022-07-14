5d1a58d6a7ab78e02066dff733a1c8e6059d0b8f
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-13 17:40:04 -0700

    Time: 133 ms (88.48%), Space: 16.1 MB (16.35%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,20 +1,39 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        nums= height
-        prefix = [i for i in height]
+        if not height: return 0
+        res = 0
+        left, right = 0, len(height) - 1
         
-        for i in range(1,len(height)):
-            prefix[i] = max(prefix[i-1], nums[i-1])
+        leftMaxHeight, rightMaxHeight = height[left], height[right]
         
-        suffix = [i for i in height]
+        while left < right:
+            if leftMaxHeight <= rightMaxHeight:
+                left += 1
+                temp = leftMaxHeight - height[left] if leftMaxHeight - height[left] >= 0 else 0
+                res += temp
+                leftMaxHeight = max(leftMaxHeight, height[left])
+            else:
+                right -= 1
+                temp = rightMaxHeight - height[right] if rightMaxHeight - height[right] >= 0 else 0
+                res += temp
+                rightMaxHeight = max(rightMaxHeight, height[right])
+        return res
+    
+#         nums= height
+#         prefix = [i for i in height]
         
-        for i in range(len(height) - 2, -1, -1):
-            suffix[i] = max(suffix[i+1], nums[i+1])
+#         for i in range(1,len(height)):
+#             prefix[i] = max(prefix[i-1], nums[i-1])
         
-        res = [0] * (len(height) )
+#         suffix = [i for i in height]
         
-        for i in range(len(height)):
-            temp = min(prefix[i], suffix[i]) - nums[i]
-            res[i] = temp if temp >= 0 else 0
+#         for i in range(len(height) - 2, -1, -1):
+#             suffix[i] = max(suffix[i+1], nums[i+1])
         
-        return sum(res)
+#         res = [0] * (len(height) )
+        
+#         for i in range(len(height)):
+#             temp = min(prefix[i], suffix[i]) - nums[i]
+#             res[i] = temp if temp >= 0 else 0
+        
+#         return sum(res)
