9aef6de76ebf81f26c17423b0220e0f5f45eee47
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-14 14:35:38 -0700

    Time: 148 ms (82.37%), Space: 14.1 MB (19.59%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,15 +1,22 @@
 from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        #the windowlength - the maxFreq decides how many characters we need to replace in that window
+        #update the windowLength after the left pointer changes
+        # technically we do not have to update the maxFreq after the left pointer changes because we only
+        # care when the maxFreq increases because that is what will have an effect on the solution
+        # if we check the hashmap for largest each time left moves the time complexity becomes O(26*N)
+        # if we do not check it becomes a pure O(N) solution
         cache = {}
-        left = 0
-        res = 0
-        maxf = 0
+        left, res = 0, 0
+        maxFreq = 0
         for right in range(len(s)):
+            windowLen = right - left + 1
             cache[s[right]] = 1 + cache.get(s[right],0)
-            maxf = max(maxf, cache[s[right]])
-            if (right - left + 1 - maxf) > k:
+            maxFreq = max(maxFreq, cache[s[right]])
+            if (windowLen - maxFreq) > k:
                 cache[s[left]] -= 1
                 left += 1
             res = max(res, right - left + 1)
         return res
+        
