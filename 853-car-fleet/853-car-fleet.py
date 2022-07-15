ccf0e6cce2e34afa950f39c8173645b65dab27e5
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-15 14:31:35 -0700

    Time: 1572 ms (38.64%), Space: 34 MB (61.82%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -1,13 +1,17 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # cars = [(position[i],speed[i]) for i in range(len(position))]
-        pairs = [[p,s] for p,s in zip(position, speed)]
-        pairs.sort(key = lambda x: x[0])
+        # we need to sort the array by time
+        # we look at it in reverse also
+        
+        cars = [[p,s] for p,s in zip(position, speed)]
         stack = collections.deque([])
-        for d,s in pairs[::-1]:
-            stack.append((target - d) / s)
-            if len(stack) >= 2 and stack[-2] >= stack[-1]:
+        
+        for p,s in sorted(cars)[::-1]:
+            time = (target - p) / s
+            stack.append(time)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                # this is not a while loop, we are checking and merging at each step so only need to compare top two 
                 stack.pop()
         return len(stack)
-            
+        
         
