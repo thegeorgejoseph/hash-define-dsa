1320d50e80322afa53cd0e450135bea2855008de
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-15 15:35:53 -0700

    Time: 29 ms (96.78%), Space: 14 MB (11.66%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -6,32 +6,16 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-#         iterative solutions are almost always better as long as you can do it
-#         no performance gain in this scenario because simple problem statement
-
-#         if not root:
-#             return None
         
-#         queue = collections.deque([root])
-#         while queue:
-#             current = queue.popleft()
-#             current.left, current.right = current.right, current.left
-#             if current.left:
-#                 queue.append(current.left)
-#             if current.right:
-#                 queue.append(current.right)
+        def recursive(node):
+            if not node:
+                return 
+            
+            node.left, node.right = node.right, node.left 
+            recursive(node.left)
+            recursive(node.right)
         
-#         return root
-
-#         recursive solution of a tree
-#         O(n) time because every node has to be visited once, O(n) space because O(h) call stack and h == n in this scenario because everything will be in the call stack once.
-        if not root:
-            return None
-        
-        left = self.invertTree(root.left)
-        right = self.invertTree(root.right)
-        root.left, root.right = right, left
-        
+        recursive(root)
         return root
         
         
