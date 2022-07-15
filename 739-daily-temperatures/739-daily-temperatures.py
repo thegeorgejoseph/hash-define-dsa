5e160bf3760aade217be6a87c4d0dd91b47fd3f2
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-14 18:03:30 -0700

    Time: 1969 ms (33.03%), Space: 25.9 MB (8.88%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,12 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = collections.deque([])
-        res = [0] * len(temperatures)
+        res = [0] * (len(temperatures))
         
         for i, t in enumerate(temperatures):
-            # if len(stack) > 0 and t > stack[-1][0]:
-            while len(stack) > 0 and t > stack[-1][0]:
-                topValue, topIdx = stack.pop()
-                res[topIdx] = i - topIdx
+            while len(stack) > 0 and stack[-1][0] < t:
+                temp, index = stack.pop()
+                res[index] = i - index
             stack.append((t,i))
+        
         return res
