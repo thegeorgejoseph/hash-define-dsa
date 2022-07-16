1af6de172ba48c2fad33050fd3494dea6e68de78
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-16 10:00:37 -0700

    Time: 100 ms (25.40%), Space: 18.6 MB (61.49%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -6,23 +6,18 @@
 #         self.right = right
 class Solution:
     def dfs(self, node, res):
-        if  res[0] == False:
-            return 0
         if not node:
             return 0
         
-        left = self.dfs(node.left,res)
-        right = self.dfs(node.right,res)
+        left = self.dfs(node.left, res)
+        right = self.dfs(node.right, res)
         
-        if abs(right - left) > 1:
+        if abs(left - right) > 1:
             res[0] = False
             
-        return max(left + 1,right + 1)
-    
-        
+        return max(left + 1, right + 1)
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        # for every node being traversed back up we need to calculate the height of the lefttree, righttree and the max of both that should be returned back up to the parent
         res = [True]
         
-        self.dfs(root,res)
-        return res[0]
+        self.dfs(root, res)
+        return False if res[0] == False else True
