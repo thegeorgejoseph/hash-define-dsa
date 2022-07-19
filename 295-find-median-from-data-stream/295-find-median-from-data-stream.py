51b1a85af6204ed0286ee31c938db66b433bc50f
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-19 16:56:09 -0700

    Time: 721 ms (66.42%), Space: 35.6 MB (93.66%) - LeetHub

---
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -0,0 +1,32 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small, self.large = [], []
+        heapq.heapify(self.small) #maxHeap
+        heapq.heapify(self.large) #minHeap
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -1 * num)
+        
+        if (self.small and self.large and (-1 * self.small[0] > self.large[0])):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        if len(self.small) > 1 + len(self.large):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > 1 + len(self.small):
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        return (-1 * self.small[0] + self.large[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
