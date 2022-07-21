09e7328924c9feb9d863ba10bfaa23d82f8774e7
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-21 16:31:19 -0700

    Time: 510 ms (42.33%), Space: 22.1 MB (15.32%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,28 +2,56 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
+        
         ROWS, COLS = len(grid), len(grid[0])
         visit = set()
-        islands = 0
-        
-        def bfs(r,c):
-            queue = collections.deque()
+        res = 0
+        def dfs(r,c):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS or (r,c) in visit or grid[r][c] == "0"):
+                return
+            
             visit.add((r,c))
-            queue.append((r,c))
-            while queue:
-                row, col = queue.popleft() #change this to pop for DFS
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-                for dr, dc in directions:
-                    r,c = row + dr, col + dc
-                    if (r in range(ROWS) and c in range(COLS) and grid[r][c] != "0" and (r,c) not in visit):
-                        visit.add((r,c))
-                        queue.append((r,c))
-        
-        
+            directions = [[0,1],[1,0],[-1,0],[0,-1]]
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+            
+    
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1" and (r,c) not in visit:
-                    bfs(r,c)
-                    islands += 1
-        return islands
+                    res += 1
+                    dfs(r,c)
+        return res
+        
+        
+        
+        
+        
+        
+#         if not grid:
+#             return 0
+#         ROWS, COLS = len(grid), len(grid[0])
+#         visit = set()
+#         islands = 0
+        
+#         def bfs(r,c):
+#             queue = collections.deque()
+#             visit.add((r,c))
+#             queue.append((r,c))
+#             while queue:
+#                 row, col = queue.popleft() #change this to pop for DFS
+#                 directions = [[1,0],[-1,0],[0,1],[0,-1]]
+#                 for dr, dc in directions:
+#                     r,c = row + dr, col + dc
+#                     if (r in range(ROWS) and c in range(COLS) and grid[r][c] != "0" and (r,c) not in visit):
+#                         visit.add((r,c))
+#                         queue.append((r,c))
+        
+        
+#         for r in range(ROWS):
+#             for c in range(COLS):
+#                 if grid[r][c] == "1" and (r,c) not in visit:
+#                     bfs(r,c)
+#                     islands += 1
+#         return islands
             
