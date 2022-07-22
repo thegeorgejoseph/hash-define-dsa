28a0cbb46db67f80ef92e04965ed87c5400b831a
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-22 15:30:59 -0700

    Time: 432 ms (54.53%), Space: 15.6 MB (78.66%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,29 +1,28 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        pacv, atlv = set(), set()
-        
-        def dfs(r,c,visit,prevHeight):
-            if ((r,c) in visit or r < 0 or r == ROWS or c < 0 or c == COLS or heights[r][c] < prevHeight):
+        pacific, atlantic = set(), set()
+        res = []
+        def dfs(r,c, visit, previousHeight):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS or (r,c) in visit or previousHeight > heights[r][c]):
                 return
-            
             visit.add((r,c))
             directions = [[1,0],[0,1],[-1,0],[0,-1]]
             for dr, dc in directions:
-                row, col = r + dr, c + dc
-                dfs(row, col, visit, heights[r][c])
+                dfs(r + dr, c + dc, visit, heights[r][c])
             
         
         for c in range(COLS):
-            dfs(0,c,pacv,heights[0][c])
-            dfs(ROWS-1,c,atlv,heights[ROWS-1][c])
-            
+            dfs(0,c,pacific,heights[0][c])
+            dfs(ROWS-1,c,atlantic,heights[ROWS-1][c])
+            # the starting position of DFS is usually the left most element
+        
         for r in range(ROWS):
-            dfs(r,0,pacv,heights[r][0])
-            dfs(r,COLS-1,atlv,heights[r][COLS-1])
-            
-        res = []
-        for r,c in pacv:
-            if (r,c) in atlv:
+            dfs(r,0,pacific,heights[r][0])
+            dfs(r,COLS-1, atlantic, heights[r][COLS-1])
+        
+        for r,c in pacific:
+            if (r,c) in atlantic:
                 res.append([r,c])
+        
         return res
