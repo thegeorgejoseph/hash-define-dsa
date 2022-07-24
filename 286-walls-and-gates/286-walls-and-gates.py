308f37aca6c120b8e59a802df34237629ba3e5e1
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-24 12:06:33 -0700

    Time: 348 ms (71.76%), Space: 16.9 MB (76.60%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -1,34 +1,22 @@
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        # 1) Set classic BFS variables:
         ROWS, COLS = len(rooms), len(rooms[0])
-        directions = [[0,1],[1,0],[-1,0],[0,-1]]
-        queue = collections.deque()
         EMPTY = 2147483647
-        GATE = 0
-
-        # 2) Enqueue all the Gates first since
-        # Search from the gates to the empty rooms:
-        for row in range(ROWS):
-            for col in range(COLS):
-                if rooms[row][col] == 0:
-                    queue.append((row, col))
-
-        # 3) Peform BFS loop:
-        # initiate BFS from all gates at the same time:
+        queue = deque()
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    queue.append((r,c))
+        
         while queue:
-            # a) Get gateway:
-            curR, curC = queue.popleft()
-
-            # b) Record distance from neighbors to Gates:
-            for rd, cd in directions:
-                r = rd + curR
-                c = cd + curC
-
-                # c) Skip walls and check boundaries:
-                if r not in range(ROWS) or c not in range(COLS) or rooms[r][c] != EMPTY:
-                    continue
-
-                # Record actual distance on each cell:    
-                rooms[r][c] = rooms[curR][curC] + 1
-                queue.append((r, c))
+            for _ in range(len(queue)):
+                r,c = queue.popleft()
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    if (row < 0 or col < 0 or row == ROWS or col == COLS or rooms[row][col] != EMPTY):
+                        continue
+                    # because you are only moving one step at a time so if another gate finds an already updated cell, no need to replace it because the closest distance has already been recorded
+                    rooms[row][col] = 1 + rooms[r][c]
+                    queue.append((row,col))
+                
