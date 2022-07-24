34e75f8a958c90e3cda2d3ebfa444a53bac5c2f1
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-24 11:25:55 -0700

    Time: 91 ms (35.52%), Space: 13.8 MB (99.64%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,24 +1,24 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        fresh, time = 0,0
-        directions = [[0,1],[1,0],[0,-1],[-1,0]]
-        queue = collections.deque()
+        fresh, time = 0, 0
+        queue = deque()
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1: fresh += 1
                 if grid[r][c] == 2: queue.append([r,c])
         
-        while queue and fresh > 0 :
-            for _ in range(len(queue)):
-                r,c  = queue.popleft()
+        while queue and fresh > 0:
+            for i in range(len(queue)):
+                r, c = queue.popleft()
                 for dr, dc in directions:
-                    row, col = dr + r, dc + c
-                    if (row < 0 or col < 0 or row == ROWS or col == COLS or grid[row][col] != 1):
+                    row, col = r + dr, c + dc
+                    if (row < 0  or col < 0 or row == ROWS or col == COLS or grid[row][col] != 1):
                         continue
+                    fresh -= 1
                     grid[row][col] = 2
                     queue.append([row,col])
-                    fresh -= 1
             time += 1
-        
+            
         return time if fresh == 0 else -1
