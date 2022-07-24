3095b557e9e4c71bcd1f776a3afdbf50113ff059
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-24 13:05:20 -0700

    Time: 102 ms (95.64%), Space: 17.5 MB (21.89%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,27 +1,59 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        preMap = {i:[] for i in range(numCourses)}
-        for cur, pre in prerequisites:
-            preMap[cur].append(pre)
-            
-        visit, cycle = set(), set()
-        # we have a visit set here instead of changing the prereq to []
+        dependencyMap = {i: [] for i in range(numCourses)}
+        visited, cycle = set(), set()
         res = []
-        
-        def dfs(course):
-            if course in cycle: # In cycle so False
-                return False
-            if course in visit: #already visited so True
-                return True
+        for crs, dep in prerequisites:
+            dependencyMap[crs].append(dep)
             
+        def backtrack(course):
+            if course in cycle: return False
+            if course in visited: return True
+            
+            visited.add(course)
             cycle.add(course)
-            for pre in preMap[course]:
-                if not dfs(pre): return False
+            
+            for dep in dependencyMap[course]:
+                if not backtrack(dep): return False
+            
             cycle.remove(course)
-            visit.add(course)
             res.append(course)
+            dependencyMap[course] = []
             return True
+            
+            
+            
         
-        for course in range(numCourses):
-            if not dfs(course): return []
+        for course, _ in dependencyMap.items():
+            if not backtrack(course): return []
+        
         return res
+        
+        
+        
+        
+#         preMap = {i:[] for i in range(numCourses)}
+#         for cur, pre in prerequisites:
+#             preMap[cur].append(pre)
+            
+#         visit, cycle = set(), set()
+#         # we have a visit set here instead of changing the prereq to []
+#         res = []
+        
+#         def dfs(course):
+#             if course in cycle: # In cycle so False
+#                 return False
+#             if course in visit: #already visited so True
+#                 return True
+            
+#             cycle.add(course)
+#             for pre in preMap[course]:
+#                 if not dfs(pre): return False
+#             cycle.remove(course)
+#             visit.add(course)
+#             res.append(course)
+#             return True
+        
+#         for course in range(numCourses):
+#             if not dfs(course): return []
+#         return res
