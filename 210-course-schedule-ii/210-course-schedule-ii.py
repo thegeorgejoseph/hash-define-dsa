4f368bf2d9b70f05e370f34d716a97c09c24e2f8
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-24 13:06:11 -0700

    Time: 143 ms (63.26%), Space: 17.5 MB (21.89%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -21,39 +21,7 @@ class Solution:
             dependencyMap[course] = []
             return True
             
-            
-            
-        
         for course, _ in dependencyMap.items():
             if not backtrack(course): return []
         
         return res
-        
-        
-        
-        
-#         preMap = {i:[] for i in range(numCourses)}
-#         for cur, pre in prerequisites:
-#             preMap[cur].append(pre)
-            
-#         visit, cycle = set(), set()
-#         # we have a visit set here instead of changing the prereq to []
-#         res = []
-        
-#         def dfs(course):
-#             if course in cycle: # In cycle so False
-#                 return False
-#             if course in visit: #already visited so True
-#                 return True
-            
-#             cycle.add(course)
-#             for pre in preMap[course]:
-#                 if not dfs(pre): return False
-#             cycle.remove(course)
-#             visit.add(course)
-#             res.append(course)
-#             return True
-        
-#         for course in range(numCourses):
-#             if not dfs(course): return []
-#         return res
