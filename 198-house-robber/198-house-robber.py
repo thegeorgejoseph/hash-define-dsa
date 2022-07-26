f7d04da698e1c45936c4fb728ccf90b54fdd72ff
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-26 13:18:20 -0700

    Time: 50 ms (47.36%), Space: 13.8 MB (65.75%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,9 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
+        nums.append(0)
+        nums.append(0)
         
-        for n in nums:
-            tmp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = tmp
-        return tmp
+        for i in range(len(nums) - 3, -1, -1):
+            nums[i] = max(nums[i]  + nums[i + 2], nums[i + 1])
+        
+        return nums[0]
