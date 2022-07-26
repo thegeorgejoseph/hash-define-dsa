750720576e5943ce7cd0335022e1fe678a49a6ab
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-26 11:33:28 -0700

    Time: 243 ms (53.40%), Space: 15.1 MB (72.78%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,13 +1,60 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        prices = [float("inf")] * n
-        prices[src] = 0
-        for _ in range(k + 1):
-            tempPrices = prices.copy()
-            for s,d,p in flights:
-                if prices[s] == float("inf"):
-                    continue
-                if prices[s] + p < tempPrices[d]:
-                    tempPrices[d] = prices[s] + p
-            prices = tempPrices
-        return -1 if prices[dst] == float("inf") else prices[dst]
+            prices = [float("inf")] * n
+            prices[src] = 0
+            
+            for _ in range(k + 1):
+                tempPrices = prices.copy()
+                for s,d,p in flights:
+                    if prices[s] == float("inf"):
+                        continue
+                    if prices[s] + p < tempPrices[d]:
+                        tempPrices[d] = prices[s] + p
+                prices = tempPrices
+            
+            return prices[dst] if prices[dst] != float("inf") else -1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # prices = [float("inf")] * n
+        # prices[src] = 0
+        # for _ in range(k + 1):
+        #     tempPrices = prices.copy()
+        #     for s,d,p in flights:
+        #         if prices[s] == float("inf"):
+        #             continue
+        #         if prices[s] + p < tempPrices[d]:
+        #             tempPrices[d] = prices[s] + p
+        #     prices = tempPrices
+        # return -1 if prices[dst] == float("inf") else prices[dst]
