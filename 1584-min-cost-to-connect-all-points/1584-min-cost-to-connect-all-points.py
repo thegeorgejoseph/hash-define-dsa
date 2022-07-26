3b2921ea095a66d5f4b93a0a04cbc392fd75bda4
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-25 17:02:38 -0700

    Time: 5795 ms (16.95%), Space: 174.8 MB (17.89%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,31 +1,30 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        N = len(points)
-        adj = {i:[] for i in range(N)}
+        graph = {i: [] for i in range(len(points))}
         
-        for i in range(N):
+        for i in range(len(points)):
             x1, y1 = points[i]
-            for j in range(i + 1, N): # we do not have to check whats behind because we are appending both directions in one go! Smart!
+            for j in range(i + 1, len(points)):
                 x2, y2 = points[j]
-                distance = abs(x2 - x1) + abs(y2 - y1)
-                adj[i].append([distance, j])
-                adj[j].append([distance, i])
-                
+                dist = abs(x2 - x1) + abs(y2 - y1)
+                graph[i].append([dist, j])
+                graph[j].append([dist, i])
         
         cost = 0
+        minHeap = [[0,0]] # cost, node - the graph created above has a link between all possible nodes
         visit = set()
-        frontier = [[0,0]]
-        heapq.heapify(frontier)
         
-        while len(visit) < N:
-            curCost, i = heapq.heappop(frontier)
+        while len(visit) < len(points):
+            minCost, i = heapq.heappop(minHeap)
             if i in visit:
                 continue
             
             visit.add(i)
-            cost += curCost
-            for neiCost, nei in adj[i]:
+            cost += minCost
+            
+            for neiCost, nei in graph[i]:
                 if nei not in visit:
-                    heapq.heappush(frontier, [neiCost, nei])
-        
+                    heapq.heappush(minHeap,[neiCost, nei])
+            
+            
         return cost
