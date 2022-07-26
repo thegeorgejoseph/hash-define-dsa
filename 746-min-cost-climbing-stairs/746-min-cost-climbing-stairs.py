1217aacc755600fd31a3ca55475cfa5b4d149919
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-26 12:44:00 -0700

    Time: 60 ms (94.58%), Space: 13.9 MB (97.18%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,10 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # [10, 15, 20]
         cost.append(0)
-        #base case 1 : to reach the goal from the goal it takes 0 cost
-        #base case 2 : to reach the goal from the n - 1 it takes cost[n-1]
-        
         for i in range(len(cost) - 3, -1, -1):
-            cost[i] = min(cost[i] + cost[i + 1], cost[i] + cost[i + 2])
-            
+            cost[i] = min(cost[i+1],cost[i+2]) + cost[i]
+        
         return min(cost[0],cost[1])
