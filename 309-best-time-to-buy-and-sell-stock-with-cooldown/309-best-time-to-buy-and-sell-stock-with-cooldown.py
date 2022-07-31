954f1fb49fc47753deaa5c33e3206422b76879ad
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-07-31 13:53:35 -0700

    Time: 80 ms (35.92%), Space: 17.9 MB (28.46%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,22 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {}
+        cache = {}
         
-        def dfs(i, buying):
+        def dfs(i, canBuy):
             if i >= len(prices):
                 return 0
-            if (i, buying) in dp:
-                return dp[(i, buying)]
+            if (i, canBuy) in cache:
+                return cache[(i, canBuy)]
             
-            if buying:
-                buy = dfs( i + 1, not buying) - prices[i]
-                cooldown = dfs( i + 1, buying)
-                dp[(i, buying)] = max(buy, cooldown)
+            if canBuy:
+                buy = dfs(i + 1, not canBuy) - prices[i]
+                cooldown = dfs(i + 1, canBuy)
+                res = max(buy, cooldown)
             else:
-                # if you are selling then you need to 
-                sell = dfs(i + 2, not buying) + prices[i]
-                cooldown = dfs( i + 1, buying)
-                dp[(i, buying)] = max(sell, cooldown)
-            return dp[(i,buying)]
+                sell = dfs(i + 2, not canBuy) + prices[i]
+                cooldown = dfs(i + 1, canBuy)
+                res = max(sell, cooldown)
+            
+            cache[(i, canBuy)] = res
+            return res
+        
             
         return dfs(0, True)
