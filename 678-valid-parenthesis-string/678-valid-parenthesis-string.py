85cc37814684a47c4c0f955e96561a208ce37288
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-04 15:53:30 -0700

    Time: 33 ms (90.62%), Space: 14 MB (25.35%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -1,36 +1,18 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        leftMin, leftMax = 0,0
-        for c in s:
-            if c == "(":
-                leftMin, leftMax = leftMin + 1, leftMax + 1
-            elif c == ")":
-                leftMin, leftMax = leftMin - 1, leftMax - 1
+        minLeftPar, maxLeftPar = 0,0
+        for char in s:
+            if char == '(':
+                minLeftPar += 1
+                maxLeftPar += 1
+            elif char == ')':
+                minLeftPar -= 1
+                maxLeftPar -= 1
             else:
-                leftMin, leftMax = leftMin - 1, leftMax + 1
-            if leftMax < 0: return False
-            if leftMin < 0 : leftMin = 0
-        return leftMin == 0
-#         Learn this stack solution too 
-#         count = []
-#         stack = []
-# 		# we only add in (, and if we meet a ), we pop one (
-# 		# if we meet a *, we add the index into count. We first assume all * is empty string.
-#         for i, st in enumerate(s):
-#             if st == "(":
-#                 stack.append(i)
-#             elif st == ")":
-#                 if not stack and not count:
-#                     return False
-#                 elif not stack:
-#                     count.pop()
-#                 else:
-#                     stack.pop()
-#             else:
-#                 count.append(i)
-		
-# 		# we finally ensure if stack has extra (, the * in count appear after (. 
-#         while stack and count:
-#             if stack.pop() > count.pop():
-#                 return False
-#         return True if not stack else False
+                minLeftPar -= 1
+                maxLeftPar += 1
+            if minLeftPar < 0:
+                minLeftPar = 0
+            if maxLeftPar < 0:
+                return False
+        return minLeftPar == 0
