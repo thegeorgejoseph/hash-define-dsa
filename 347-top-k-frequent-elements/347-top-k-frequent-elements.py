85b0068f02653b3e2a8e4c2139c36bf256cd0ff3
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-07 17:49:57 -0700

    Time: 175 ms (43.86%), Space: 19.3 MB (29.66%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,20 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-#         Bucket sort technique
-# Make a hash-map that stores all the counts
-# then create another list if you want that stores the indices as the count and the values as lists with the numbers that are of that particular count.
-        cache = {}
-        freq = [[] for i in range(len(nums) + 1)]
-        
-        for n in nums:
-            cache[n] = cache.get(n,0) + 1
-        for n, c in cache.items():
-            freq[c].append(n)
-        
+        Counter = {}
         res = []
+        for n in nums:
+            Counter[n] = 1 + Counter.get(n, 0)
         
-        for i in range(len(nums),0,-1):
-            for n in freq[i]:
+        freqs = [[] for _ in range(len(nums) +1)]
+        for num, count in Counter.items():
+            freqs[count].append(num)
+        for i in range(len(freqs) - 1, -1, -1):
+            if not freqs[i]:
+                continue
+            for n in freqs[i]:
                 res.append(n)
                 if len(res) == k:
                     return res
+        
