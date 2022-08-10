3aad0ed4310030786abdda88bceb01bdb5d7cf31
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-10 13:51:49 -0700

    Time: 67 ms (21.65%), Space: 13.8 MB (61.15%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -21,21 +21,3 @@ class Solution:
 #             n = squareSums(n)
         
 #         return True
-             
-        
-#         variation of Floyd Warshall's Algorithm to find cycles
-
-#         def get_next(n):
-#             result = 0
-#             while n :
-#                 n, remainder = divmod(n, 10)
-#                 result += remainder ** 2
-#             return result
-        
-#         slow = n
-#         fast = get_next(n) #start the fast pointer from the next one
-#         while fast != 1 and fast != slow:
-#             slow = get_next(slow)
-#             fast = get_next(get_next(fast))
-            
-#         return fast == 1
