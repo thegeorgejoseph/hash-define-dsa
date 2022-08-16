a8c38b010e82c1973fa705d6a2fb9933ddbe597e
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-16 13:44:20 -0700

    Time: 242 ms (21.74%), Space: 17.6 MB (74.44%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -4,27 +4,11 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
-            return None
-        
-        while len(lists) > 1:
-            temp = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
-                temp.append(self.mergeTwoLists(l1,l2))
-            lists = temp
-        
-        return lists[0]
-    
-    def mergeTwoLists(self, l1, l2):
-        dummy = ListNode()
+    def mergeLists(self, l1, l2):
+        dummy = ListNode(0, None)
         current = dummy
-        
         headOne, headTwo = l1, l2
-        
-        while headOne is not None and headTwo is not None:
+        while headOne and headTwo:
             if headOne.val <= headTwo.val:
                 temp = headOne.next
                 current.next = headOne
@@ -38,8 +22,19 @@ class Solution:
         if headOne:
             current.next = headOne
         if headTwo:
-            current.next = headTwo 
-        
+            current.next = headTwo
         return dummy.next
-                
-            
+    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+        
+        while len(lists) > 1:
+            temp = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                temp.append(self.mergeLists(l1,l2))
+            lists = temp
+        
+        return lists[0]
