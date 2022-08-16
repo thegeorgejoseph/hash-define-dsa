d8d644cdb58a00938d54fd02943791c76a9f2068
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-15 21:59:52 -0700

    Time: 164 ms (27.00%), Space: 23.9 MB (64.25%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,51 +8,27 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow, fast = head, head.next 
+        
+        slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
-        curr = slow.next
+        current = slow.next
         slow.next = None
-        prev = None  
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
+        
+        prev = None
+        while current:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
         
         first, second = head, prev
         while first and second:
             temp1, temp2 = first.next, second.next
             first.next = second
             second.next = temp1
-            first = temp1
             second = temp2
-        
+            first = temp1
             
-#         slow, fast = head, head.next
-#         while fast and fast.next: #we check for fast.next so that we can keep track of the centre accurately
-#             slow = slow.next
-#             fast = fast.next.next
-        
-        
-#         current = slow.next
-#         slow.next = None # this is to turn the first halfs last link off
-#         prev = None
-#         while current:
-#             nextNode = current.next
-#             current.next = prev
-#             prev = current
-#             current = nextNode
-        
-#         #prev will hold the second head now
-#         #head will hold the original head
-        
-#         first, second = head, prev
-#         while first and second:
-#             temp1, temp2 = first.next, second.next
-#             first.next = second
-#             second.next = temp1
-#             first = temp1
-#             second = temp2
