a57afc7ccf84895fcbe9b9e8d62d79e513322f94
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-15 18:23:26 -0700

    Time: 754 ms (79.87%), Space: 28 MB (55.22%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,6 +1,5 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        #Floyd Warshall's Tortoise and Hare Algorithm in Linked Lists using Arrays
         slow, fast = 0,0
         while True:
             slow = nums[slow]
@@ -11,5 +10,4 @@ class Solution:
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
-        
         return slow
