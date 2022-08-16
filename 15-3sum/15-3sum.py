992d455f6addbd9d5c0bc67aea5f86389ec9e18f
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-15 19:39:05 -0700

    Time: 910 ms (81.18%), Space: 18.2 MB (41.07%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,21 +1,17 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        if n < 3:
+        if len(nums) < 3:
             return []
         res = []
-        nums.sort() 
-        #why do you keep forgetting to sort it?
-        for index, num in enumerate(nums):
-            # we need to ensure that we are not starting from the same number again too!
-            if index > 0 and nums[index] == nums[index-1]:
+        nums.sort()
+        for idx, num in enumerate(nums):
+            if idx > 0 and nums[idx] == nums[idx - 1]:
                 continue
-            left, right = index + 1, n - 1
+            left, right = idx + 1, len(nums) - 1
             while left < right:
-                threesum = num + nums[left] + nums[right]
-                if threesum < 0:
+                if num + nums[left] + nums[right] < 0:
                     left += 1
-                elif threesum > 0:
+                elif num + nums[left] + nums[right] > 0:
                     right -= 1
                 else:
                     res.append([num, nums[left], nums[right]])
