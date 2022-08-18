5d25738bb3ed497e53e018fbc603283aab290527
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-18 15:35:02 -0700

    Time: 543 ms (43.62%), Space: 27.1 MB (33.17%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,27 +1,10 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k
-        def quickSelect(l, r):
-            if l == r:
-                return nums[l]
-            p, pivot = l, nums[r]
-            for i in range(l, r):
-                if nums[i] <= pivot:
-                    nums[i], nums[p] = nums[p], nums[i]
-                    p += 1
-            nums[p], nums[r] = nums[r], nums[p]
-            if p > k:
-                return quickSelect(l, p - 1)
-            elif p < k:
-                return quickSelect(p + 1, r)
-            else:
-                return nums[p]
-        return quickSelect(0,len(nums)-1)
-        # return sorted(nums)[len(nums)-k]
-        # k = len(nums) - k
-        # heapq.heapify(nums)
-        # top = None
-        # while k > 0:
-        #     heapq.heappop(nums)
-        #     k -= 1
-        # return nums[0]
+        nums = [-1 * n for n in nums]
+        heapq.heapify(nums)
+        while k > 1:
+            heapq.heappop(nums)
+            k -= 1
+        print(nums)
+        return nums[0] * -1
+        # return sorted(nums)[len(nums) - k]
