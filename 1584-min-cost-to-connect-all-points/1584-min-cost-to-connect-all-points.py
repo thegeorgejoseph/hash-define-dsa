8006d31bd9295e78c21eb4fdf9d7eb62738d59df
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-18 17:11:50 -0700

    Time: 5131 ms (27.61%), Space: 173.9 MB (20.61%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,30 +1,33 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        graph = {i: [] for i in range(len(points))}
+        graph = {i : [] for i in range(len(points))}
         
         for i in range(len(points)):
-            x1, y1 = points[i]
+            x1,y1 = points[i]
             for j in range(i + 1, len(points)):
                 x2, y2 = points[j]
-                dist = abs(x2 - x1) + abs(y2 - y1)
-                graph[i].append([dist, j])
-                graph[j].append([dist, i])
+                distance = abs(x2 - x1) + abs(y2 - y1)
+                graph[i].append([distance, j])
+                graph[j].append([distance, i])
         
-        cost = 0
-        minHeap = [[0,0]] # cost, node - the graph created above has a link between all possible nodes
         visit = set()
+        minHeap = [[0,0]] # cost, index of point
+        cost = 0
         
         while len(visit) < len(points):
-            minCost, i = heapq.heappop(minHeap)
+            minDist, i = heapq.heappop(minHeap)
             if i in visit:
                 continue
             
             visit.add(i)
-            cost += minCost
+            cost += minDist
             
-            for neiCost, nei in graph[i]:
-                if nei not in visit:
-                    heapq.heappush(minHeap,[neiCost, nei])
-            
-            
+            for neiCost, j in graph[i]:
+                if j not in visit:
+                    heapq.heappush(minHeap, [neiCost, j])
+        
         return cost
+        
+        
+        
+        
