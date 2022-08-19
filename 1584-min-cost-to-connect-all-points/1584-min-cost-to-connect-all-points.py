f720af8281ccf3bbec4f29bc915c794af053f2b3
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-19 15:34:30 -0700

    Time: 4646 ms (33.30%), Space: 175 MB (8.31%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -3,31 +3,27 @@ class Solution:
         graph = {i : [] for i in range(len(points))}
         
         for i in range(len(points)):
-            x1,y1 = points[i]
+            x1, y1 = points[i]
             for j in range(i + 1, len(points)):
                 x2, y2 = points[j]
+                
                 distance = abs(x2 - x1) + abs(y2 - y1)
                 graph[i].append([distance, j])
                 graph[j].append([distance, i])
         
         visit = set()
-        minHeap = [[0,0]] # cost, index of point
+        minHeap = [[0,0]] # cost, point index
         cost = 0
         
         while len(visit) < len(points):
-            minDist, i = heapq.heappop(minHeap)
+            distance, i = heapq.heappop(minHeap)
             if i in visit:
                 continue
             
             visit.add(i)
-            cost += minDist
-            
+            cost += distance
             for neiCost, j in graph[i]:
                 if j not in visit:
-                    heapq.heappush(minHeap, [neiCost, j])
-        
+                    heapq.heappush(minHeap,[neiCost, j])
         return cost
-        
-        
-        
         
