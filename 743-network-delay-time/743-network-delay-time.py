208b4334b354672b3912146d86a63d243d7de32e
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-20 12:15:49 -0700

    Time: 732 ms (49.22%), Space: 16.8 MB (12.24%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,23 +1,25 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = defaultdict(list)
-        for u, v, w in times:
+        
+        for u,v,w in times:
             graph[u].append([w,v])
         
         time = 0
-        minHeap = [[0, k]]
+        minHeap = [[0,k]]
         visit = set()
         
+        #the graph can be disconnected
         while minHeap and len(visit) < n:
-            travelTime, dest = heapq.heappop(minHeap)
-            if dest in visit:
+            cost, i = heapq.heappop(minHeap)
+            if i in visit:
                 continue
             
-            visit.add(dest)
-            time = max(time, travelTime)
+            visit.add(i)
+            time = max(time, cost)
             
-            for neiCost, j in graph[dest]:
+            for neiCost, j in graph[i]:
                 if j not in visit:
-                    heapq.heappush(minHeap, [neiCost + time, j])
+                    heapq.heappush(minHeap,[cost + neiCost, j])
         
         return time if len(visit) == n else -1
