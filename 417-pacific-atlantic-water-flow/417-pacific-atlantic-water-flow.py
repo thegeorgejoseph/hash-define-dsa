e46655a58c73928f85ad64574fafc3b631b6e601
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-21 12:19:18 -0700

    Time: 569 ms (24.36%), Space: 15.4 MB (95.19%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,28 +1,30 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        pacific, atlantic = set(), set()
-        res = []
-        def dfs(r,c, visit, previousHeight):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS or (r,c) in visit or previousHeight > heights[r][c]):
-                return
+        pac, atl = set(), set()
+        res= []
+        def dfs(r,c,previousHeight, visit):
+            if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or previousHeight > heights[r][c]):
+                return 
+            
             visit.add((r,c))
             directions = [[1,0],[0,1],[-1,0],[0,-1]]
             for dr, dc in directions:
-                dfs(r + dr, c + dc, visit, heights[r][c])
-            
+                row, col = r + dr, c + dc
+                dfs(row, col, heights[r][c], visit)
+        
+            return            
         
         for c in range(COLS):
-            dfs(0,c,pacific,heights[0][c])
-            dfs(ROWS-1,c,atlantic,heights[ROWS-1][c])
-            # the starting position of DFS is usually the left most element
+            dfs(0,c,heights[0][c],pac)
+            dfs(ROWS-1,c,heights[ROWS-1][c],atl)
         
         for r in range(ROWS):
-            dfs(r,0,pacific,heights[r][0])
-            dfs(r,COLS-1, atlantic, heights[r][COLS-1])
+            dfs(r, 0, heights[r][0], pac)
+            dfs(r, COLS-1, heights[r][COLS-1], atl)
         
-        for r,c in pacific:
-            if (r,c) in atlantic:
+        for r,c in pac:
+            if (r,c) in atl:
                 res.append([r,c])
         
         return res
