13631549db52b9038365f478a832644a5138e8f8
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-21 12:38:21 -0700

    Time: 226 ms (52.22%), Space: 15.8 MB (71.58%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -2,26 +2,28 @@ class Solution:
     def solve(self, board: List[List[str]]) -> None:
         ROWS, COLS = len(board), len(board[0])
         
-        
         def dfs(r,c):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != "O"):
-                return
+            if (r < 0 or r == ROWS or c < 0 or c == COLS or board[r][c] != "O"):
+                return 
             
             board[r][c] = "T"
-            directions = [[1,0],[0,1],[-1,0],[0,-1]]
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
+            dfs(r, c +1)
+            dfs(r, c - 1)
+            dfs(r + 1,c)
+            dfs(r - 1,c)
             
-            
+        
         
         for r in range(ROWS):
             for c in range(COLS):
-                if ((r in [0,ROWS-1] or c in [0,COLS-1]) and board[r][c] == "O"):
-                    dfs(r,c)
+                if r in [0,ROWS-1] or c in [0,COLS-1]:
+                    if board[r][c] == "O":
+                        dfs(r,c)
         
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == "O": board[r][c] = "X"
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "T": board[r][c] = "O"
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+                elif board[r][c] == "O":
+                    board[r][c] = "X"
+    
