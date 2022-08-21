791438e401b6f36c42b18b6cf361fbd8fc357135
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-21 15:56:49 -0700

    Time: 129 ms (73.02%), Space: 17.1 MB (25.94%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,24 +1,28 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        dependencyMap = {i : [] for i in range(numCourses)}
-        visit = set()
-        for course, prereq in prerequisites:
-            dependencyMap[course].append(prereq)
+        graph = {i : [] for i in range(numCourses)}
+        cycle = set()
+        
+        for course, pre in prerequisites:
+            graph[course].append(pre)
         
         def backtrack(course):
-            if course in visit:
+            if course in cycle:
                 return False
-            if not dependencyMap[course]:
+            if not graph[course]:
                 return True
-            visit.add(course)
-            for crs in dependencyMap[course]:
-                if not backtrack(crs): return False
-            visit.remove(course)
-            dependencyMap[course] = []
+            
+            cycle.add(course)
+            for pre in graph[course]:
+                if not backtrack(pre): return False
+            
+            cycle.remove(course)
+            graph[course] = []
             return True
-            
-            
-            
-        for course, _ in dependencyMap.items():
+        
+        
+        
+        for course, pre in graph.items():
             if not backtrack(course): return False
+        
         return True
