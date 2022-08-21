4f3c80cf405eb3406bc29440a9ed21d512b1910b
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-21 15:22:44 -0700

    Time: 336 ms (75.34%), Space: 17.2 MB (51.51%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -1,22 +1,20 @@
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         ROWS, COLS = len(rooms), len(rooms[0])
-        EMPTY = 2147483647
         queue = deque()
+        EMPTY = 2147483647
         directions = [[0,1],[1,0],[-1,0],[0,-1]]
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
                     queue.append((r,c))
-        
-        while queue:
+        while queue:    
             for _ in range(len(queue)):
                 r,c = queue.popleft()
                 for dr, dc in directions:
                     row, col = r + dr, c + dc
-                    if (row < 0 or col < 0 or row == ROWS or col == COLS or rooms[row][col] != EMPTY):
+                    if (row < 0 or row == ROWS or col < 0 or col == COLS or rooms[row][col] != EMPTY):
                         continue
-                    # because you are only moving one step at a time so if another gate finds an already updated cell, no need to replace it because the closest distance has already been recorded
-                    rooms[row][col] = 1 + rooms[r][c]
+                    rooms[row][col] = rooms[r][c] + 1
                     queue.append((row,col))
-                
+        
