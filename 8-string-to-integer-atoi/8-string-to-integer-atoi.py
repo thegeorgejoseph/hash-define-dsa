bdc0b8c6770952a5cf8026c7931738898597496e
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-22 12:46:06 -0700

    Time: 83 ms (5.17%), Space: 13.9 MB (79.88%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -3,12 +3,14 @@ class Solution:
         # how to find convert a string number to integer? -> use ord('char') - ord('a') to get actual integer number
         # how to create a number from left to right? -> use char * 10 + new char to get the number, try with 789 example. -> This handles leading 0s case automatically.
         # how to strip leading and trailing spaces? -> use Python.strip()
+        # after the strip is done the resulting string could be empty so return 0
         s = s.strip()
+        if not s:
+            return 0
         negative = False
         INT_MIN, INT_MAX = -pow(2,31), pow(2,31) - 1
         res = 0
-        if not s:
-            return 0
+        
         if s[0] == "-":
             negative = True
         elif s[0] == "+":
@@ -20,17 +22,49 @@ class Solution:
         
         for i in range(1, len(s)):
             if s[i].isnumeric():
-                res = res*10 + (ord(s[i]) - ord("0"))
-                if res > INT_MAX and negative:
+                res = res*10 + (ord(s[i])-ord("0"))
+                if negative and res > INT_MAX:
                     return INT_MIN
-                elif res > INT_MAX and not negative:
+                elif res > INT_MAX:
                     return INT_MAX
-                
             else:
                 break
-        
-        if not negative:
-            return res
-        else:
+            
+        if negative:
             return -res
+        else:
+            return res
+            
+            
+            
+#         s = s.strip()
+#         negative = False
+#         INT_MIN, INT_MAX = -pow(2,31), pow(2,31) - 1
+#         res = 0
+#         if not s:
+#             return 0
+#         if s[0] == "-":
+#             negative = True
+#         elif s[0] == "+":
+#             negative = False
+#         elif not s[0].isnumeric():
+#             return 0
+#         else:
+#             res = ord(s[0]) - ord("0")
+        
+#         for i in range(1, len(s)):
+#             if s[i].isnumeric():
+#                 res = res*10 + (ord(s[i]) - ord("0"))
+#                 if res > INT_MAX and negative:
+#                     return INT_MIN
+#                 elif res > INT_MAX and not negative:
+#                     return INT_MAX
+                
+#             else:
+#                 break
+        
+#         if not negative:
+#             return res
+#         else:
+#             return -res
         
