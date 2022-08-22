b1cf682cb11cbd71eff01aaaa8908d14b3916cf9
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-21 19:56:36 -0700

    Time: 144 ms, Memory: 19 MB - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,9 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_strs = [sorted(tuple(w)) for w in strs]
         hashmap = defaultdict(list)
-        
-        for i,w in enumerate(sorted_strs):
-            hashmap[tuple(w)].append(strs[i])
+        for word in strs:
+            hashes = [0] * 26
+            for c in word:
+                hashes[ord(c) - ord('a')] += 1
+            hashmap[tuple(hashes)].append(word)
         
         return list(hashmap.values())
+#         sorted_strs = [sorted(tuple(w)) for w in strs]
+#         hashmap = defaultdict(list)
+        
+#         for i,w in enumerate(sorted_strs):
+#             hashmap[tuple(w)].append(strs[i])
+        
+#         return list(hashmap.values())
