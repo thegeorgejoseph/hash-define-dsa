65595521e6aebd9999a5c5e1e3d729f43c4db998
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-21 19:15:58 -0700

    Time: 115 ms, Memory: 17.1 MB - LeetHub

---
+++ course-schedule/course-schedule.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = {i : [] for i in range(numCourses)}
+        cycle = set()
+        for crs, pre in prerequisites:
+            graph[crs].append(pre)
+        
+        def backtrack(course):
+            if course in cycle: return False
+            if not graph[course]: return True
+            
+            cycle.add(course)
+            for pre in graph[course]:
+                if not backtrack(pre): return False
+            
+            cycle.remove(course)
+            graph[course] = []
+            return True
+            
+        
+        for course, pre in graph.items():
+            if not backtrack(course): return False
+        
+        return True
