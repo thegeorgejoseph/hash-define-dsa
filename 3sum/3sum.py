3141f3588b665a2039d6c9d9ec77cce6c5c697bd
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-22 13:10:41 -0700

    Time: 1572 ms, Memory: 18.1 MB - LeetHub

---
+++ 3sum/3sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for idx, num in enumerate(nums):
+            if idx > 0 and num == nums[idx-1]:
+                continue
+            left, right = idx + 1, len(nums) - 1
+            while left < right:
+                if num + nums[left] + nums[right] < 0:
+                    left += 1
+                elif num + nums[left] + nums[right] > 0:
+                    right -= 1
+                else:
+                    res.append([num, nums[left], nums[right]])
+                    left += 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+        return res
