c8ed3a3d9e38eadf6ed9725751c440a614fb08a5
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-23 08:42:47 -0700

    Time: 42 ms, Memory: 13.8 MB - LeetHub

--- rotate-image/rotate-image.py
+++ rotate-image/rotate-image.py
@@ -6,10 +6,11 @@ class Solution:
             for i in range(right - left):
                 temp = matrix[top][left + i]
                 
-                matrix[top][left +i] = matrix[bottom -i][left]
-                matrix[bottom-i][left] = matrix[bottom][right -i]
-                matrix[bottom][right -i] = matrix[top+i][right]
-                matrix[top+i][right] = temp
+                matrix[top][top + i] = matrix[bottom - i][left]
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+                matrix[bottom][right - i] = matrix[top + i][right]
+                matrix[top + i][right] = temp
             left += 1
             right -= 1
+        
         
