05d7edb6a75a16d9d0abac4029bc379de4b97caf
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-24 16:50:13 -0700

    Time: 105 ms (59.19%), Space: 25.9 MB (88.35%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -12,13 +12,16 @@ class Solution:
             if not node:
                 return None
             
-            if node == p or node == q: return node
+            if node == p or node == q:
+                return node
             
             left = dfs(node.left)
             right = dfs(node.right)
             
-            if left and right: return node
+            if left and right:
+                return node
             else:
                 return left or right
         
         return dfs(root)
+            
