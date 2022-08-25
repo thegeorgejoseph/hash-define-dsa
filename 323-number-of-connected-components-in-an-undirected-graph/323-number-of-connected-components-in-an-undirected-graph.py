a2244fee8b7a7c2a4736558404087898bb2c1800
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-24 19:50:40 -0700

    Time: 128 ms (74.51%), Space: 15.5 MB (50.02%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,8 +1,8 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        parent = [i for i in range(n)]
-        rank = [1] * (n)
         components = n
+        parent = [i for i in range(n)]
+        rank = [1] * n
         
         def find(n):
             p = parent[n]
@@ -11,8 +11,8 @@ class Solution:
                 p = parent[p]
             return p
         
-        def union(v1, v2):
-            p1, p2 = find(v1), find(v2)
+        def union(n1,n2):
+            p1, p2 = find(n1), find(n2)
             
             if p1 == p2:
                 return 0
@@ -23,7 +23,9 @@ class Solution:
             else:
                 parent[p1] = p2
                 rank[p2] += rank[p1]
+            
             return 1
+            
         
         for v1, v2 in edges:
             components -= union(v1,v2)
