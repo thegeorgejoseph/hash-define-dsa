5305c3c48b9a1dd219171fa03f0e9724d64d1cf0
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-28 10:53:08 -0700

    Time: 884 ms (27.33%), Space: 17.8 MB (45.42%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -3,24 +3,50 @@ class Solution:
         ROWS, COLS = len(matrix), len(matrix[0])
         dp = {}
         
-        def dfs(r,c,prev):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS or prev >= matrix[r][c]):
+        def dfs(r,c, prev):
+            if (r < 0 or r == ROWS or c < 0 or c == COLS or matrix[r][c] <= prev):
                 return 0
-            if (r,c) in dp:
-                return dp[(r,c)]
+            if (r,c) in dp: return dp[(r,c)]
             
             res = 1
-            res = max(res, dfs(r + 1,c, matrix[r][c]) + 1)
-            res = max(res, dfs(r,c + 1, matrix[r][c]) + 1)
-            res = max(res, dfs(r - 1,c, matrix[r][c]) + 1)
-            res = max(res, dfs(r,c - 1, matrix[r][c]) + 1)
-            
+            res = max(res, dfs(r + 1, c, matrix[r][c]) + 1)
+            res = max(res, dfs(r, c + 1, matrix[r][c]) + 1)
+            res = max(res, dfs(r - 1, c, matrix[r][c]) + 1)
+            res = max(res, dfs(r, c - 1, matrix[r][c]) + 1)
             dp[(r,c)] = res
             return res
+        
         
         LIP = 1
         for r in range(ROWS):
             for c in range(COLS):
-                LIP = max(LIP, dfs(r,c,-1))
+                LIP = max(LIP, dfs(r,c, float("-inf")))
         
         return LIP
+        
+        
+        
+#         ROWS, COLS = len(matrix), len(matrix[0])
+#         dp = {}
+        
+#         def dfs(r,c,prev):
+#             if (r < 0 or c < 0 or r == ROWS or c == COLS or prev >= matrix[r][c]):
+#                 return 0
+#             if (r,c) in dp:
+#                 return dp[(r,c)]
+            
+#             res = 1
+#             res = max(res, dfs(r + 1,c, matrix[r][c]) + 1)
+#             res = max(res, dfs(r,c + 1, matrix[r][c]) + 1)
+#             res = max(res, dfs(r - 1,c, matrix[r][c]) + 1)
+#             res = max(res, dfs(r,c - 1, matrix[r][c]) + 1)
+            
+#             dp[(r,c)] = res
+#             return res
+        
+#         LIP = 1
+#         for r in range(ROWS):
+#             for c in range(COLS):
+#                 LIP = max(LIP, dfs(r,c,-1))
+        
+#         return LIP
