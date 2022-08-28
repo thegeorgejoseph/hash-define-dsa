3dc475b310e14ac8c51ed0907c62c8a3727aa7ae
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-27 22:54:13 -0700

    Time: 674 ms (52.60%), Space: 22.6 MB (65.36%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,6 +6,5 @@ class Solution:
                 if text1[i] == text2[j]:
                     dp[i][j] = 1 + dp[i + 1][j + 1]
                 else:
-                    dp[i][j] = max(dp[i + 1][j],dp[i][j + 1])
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
         return dp[0][0]
-    
