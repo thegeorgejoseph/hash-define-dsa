6394d5aca4420dcaa64ac160994a14d1fb325240
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-28 19:21:08 -0700

    Time: 135 ms (73.32%), Space: 18.7 MB (47.95%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -2,17 +2,29 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         Counter = {}
         res = []
-        for n in nums:
-            Counter[n] = 1 + Counter.get(n, 0)
+        for num in nums:
+            Counter[num] = Counter.get(num, 0) + 1
         
-        freqs = [[] for _ in range(len(nums) +1)]
-        for num, count in Counter.items():
-            freqs[count].append(num)
-        for i in range(len(freqs) - 1, -1, -1):
-            if not freqs[i]:
-                continue
-            for n in freqs[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        maxHeap = []
+        for key, value in Counter.items():
+            heapq.heappush(maxHeap, (-value, key))
         
+        while maxHeap:
+            value, key = heapq.heappop(maxHeap)
+            res.append(key)
+            if len(res) == k:
+                return res
+        return res
+        
+        # Counter = {}
+        # res = []
+        # for n in nums:
+        #     Counter[n] = 1 + Counter.get(n, 0)
+        # freqs = [[] for i in range(len(nums) +1)]
+        # for n, count in Counter.items():
+        #     freqs[count].append(n)
+        # for i in range(len(freqs)-1,-1,-1):
+        #     for n in freqs[i]:
+        #         res.append(n)
+        #         if len(res) == k:
+        #             return res
