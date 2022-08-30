cbfa3b99855b24d4e5940e01dd39afef4a687530
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-08-30 12:14:02 -0700

    Time: 97 ms (78.76%), Space: 17.3 MB (92.22%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,15 +1,20 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # three conditions 
+        # condition to place the new interval before the rest of the array
+        # condition to place the new interval after the rest of the array
+        # condition to place the new interval as a merged array
         res = []
-        
         for i in range(len(intervals)):
-            if newInterval[0] < intervals[i][0] and newInterval[1] < intervals[i][0]:
+            currentInterval = intervals[i]
+            if newInterval[0] < currentInterval[0] and newInterval[1] < currentInterval[0]:
                 res.append(newInterval)
-                return res + intervals[i :]
-            if newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
-            elif newInterval[0] >= intervals[i][0] or newInterval[1] <= intervals[i][1]:
-                newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
-            
+                return res + intervals[i : ]
+            elif newInterval[0] > currentInterval[1]:
+                res.append(currentInterval)
+            else:
+                newInterval = [min(newInterval[0], currentInterval[0]),max(newInterval[1], currentInterval[1])]
         res.append(newInterval)
         return res
+    
+    
