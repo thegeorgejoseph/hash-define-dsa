5ed2a27c78ddfd421204913dfde45eb0ac7712e5
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-02 20:51:24 -0700

    Time: 452 ms (59.43%), Space: 21.9 MB (25.48%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,49 +1,35 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
         ROWS, COLS = len(grid), len(grid[0])
+        directions = [[0,1],[1,0],[0,-1],[-1,0]]
         visit = set()
-        count = 0
-        queue = deque([])
         
-        def bfs():
+        # def dfs(r,c):
+        #     if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] != "1"):
+        #         return 
+        #     visit.add((r,c))
+        #     for dr, dc in directions:
+        #         row, col = r + dr, c + dc
+        #         dfs(row, col)
+        def bfs(r,c):
+            queue = deque([(r,c)])
             while queue:
                 r,c = queue.popleft()
                 if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] != "1"):
                     continue
                 visit.add((r,c))
-                directions = [[1,0],[0,1],[-1,0],[0,-1]]
                 for dr, dc in directions:
                     row, col = r + dr, c + dc
-                    queue.append([row,col])
-        
+                    queue.append((row, col))
+                    
+            
+        islands = 0 
         for r in range(ROWS):
             for c in range(COLS):
                 if (r,c) not in visit and grid[r][c] == "1":
-                    queue.append([r,c])
-                    bfs()
-                    count += 1
+                    islands += 1
+                    # dfs(r,c)
+                    bfs(r,c)
+        return islands
         
-        return count
-#         ROWS, COLS = len(grid), len(grid[0])
-#         visit = set()
-#         count = 0
         
-#         def dfs(r,c):
-#             if (r < 0 or r == ROWS or c < 0 or c == COLS or grid[r][c] != "1" or (r,c) in visit):
-#                 return 
-            
-#             visit.add((r,c))
-#             directions = [[1,0],[0,1],[-1,0],[0,-1]]
-#             for dr, dc in directions:
-#                 row, col = r + dr, c + dc
-#                 dfs(row, col)
-            
-        
-#         for i in range(ROWS):
-#             for j in range(COLS):
-#                 if (i,j) not in visit and grid[i][j] == "1":
-#                     dfs(i, j)
-#                     count += 1
-        
-#         return count
