a9d78682f1bc3348a832a4dc5c5f33ae54552cbb
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-04 13:01:53 -0700

    Time: 55 ms (88.54%), Space: 13.9 MB (80.10%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), 
+                  (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), 
+                  (5, "V"), (4, "IV"), (1, "I")]
+        res = []
+        for divisor, char in digits:
+            count, num = divmod(num, divisor)
+            res.append(char*count)
+        return "".join(res)
