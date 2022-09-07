dacae8a2bcd34383b264b04d5c3f4f32c2a282cf
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-06 23:12:16 -0700

    Time: 1530 ms (49.23%), Space: 33.6 MB (82.74%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -1,17 +1,15 @@
 class Solution:
-    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # we need to sort the array by time
-        # we look at it in reverse also
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:    
+        cars = [[position[i],speed[i]] for i in range(len(position))]
+        # we need to sort the cars by position but also remember the corresponding times
+        stack = []
         
-        cars = [[p,s] for p,s in zip(position, speed)]
-        stack = collections.deque([])
-        
-        for p,s in sorted(cars)[::-1]:
+        for p, s in sorted(cars)[::-1]:
+            # if we start the cars that are closest to the target and then insert newer cars that take less time then they will actually match the time of the slower car so we can just pop the quicker (new one that was added) one
             time = (target - p) / s
             stack.append(time)
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                # this is not a while loop, we are checking and merging at each step so only need to compare top two 
+                # there will be at least one fleet with how the question has been set up
                 stack.pop()
+        
         return len(stack)
-        
-        
