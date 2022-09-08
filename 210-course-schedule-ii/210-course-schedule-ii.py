b4bc94ecfbd144bfe3dfdc9fa0d6820c74a072cb
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-08 16:20:52 -0700

    Time: 241 ms (12.51%), Space: 17.6 MB (11.33%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,31 +1,30 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = {i: [] for i in range(numCourses)}
-        res = []
-        visit, cycle = set(), set()
-        
+        preMap = {i : [] for i in range(numCourses)}
         for crs, pre in prerequisites:
-            graph[crs].append(pre)
+            preMap[crs].append(pre)
         
+        cycle, visit = set(), set()
+        res = []
         
-        def backtrack(course):
+        def dfs(course):
             if course in cycle:
                 return False
             if course in visit:
                 return True
             
+            
             cycle.add(course)
             visit.add(course)
-            for pre in graph[course]:
-                if not backtrack(pre): return False
-            
-            cycle.remove(course)
+            for pre in preMap[course]:
+                if not dfs(pre): return False
+            preMap[course] = []
             res.append(course)
-            graph[course] = []
+            cycle.remove(course)
             return True
         
         
-        for crs, pre in graph.items():
-            if not backtrack(crs): return []
+        for course, _ in preMap.items():
+            if not dfs(course): return []
         
         return res
