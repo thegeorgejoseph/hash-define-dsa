fa2b6a06f22cd64ba1e945e136eae375ef017cf1
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-09 11:39:10 -0700

    Time: 609 ms (5.01%), Space: 22.5 MB (18.45%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,25 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        prefix_array = []
-        pre = 1
-        for i in range(n):
-            prefix_array.append(pre * nums[i])
-            pre = prefix_array[-1]
-        post_array = []
-        post = 1
-        for i in range(n-1,-1,-1):
-            post_array.append(post * nums[i])
-            post = post_array[-1]
-        post_array.reverse()
+        prefix_sum  = []
+        prefix = 1
+        for num in nums:
+            prefix_sum.append(prefix * num)
+            prefix = prefix * num
+        suffix_sum = []
+        suffix = 1
+        for num in nums[::-1]:
+            suffix_sum.append(suffix * num)
+            suffix = suffix * num
+        suffix_sum = suffix_sum[::-1]
         
         res = []
-        for i in range(n):
+        for i in range(len(nums)):
             if i == 0:
-                res.append(1 * post_array[i + 1])
-            elif i == n - 1:
-                res.append(1 * prefix_array[i-1])
+                res.append(suffix_sum[i+1])
+            elif i == len(nums)-1:
+                res.append(prefix_sum[i-1])
             else:
-                res.append(prefix_array[i-1] * post_array[ i + 1])
+                res.append(prefix_sum[i-1]*suffix_sum[i+1])
         return res
-            
