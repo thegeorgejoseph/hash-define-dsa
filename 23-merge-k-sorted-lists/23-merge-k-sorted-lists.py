c0132ccd0b6aabb1def91c6768bc10aa9af3d981
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-09 12:23:23 -0700

    Time: 242 ms (22.71%), Space: 17.6 MB (74.11%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -4,39 +4,35 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeLists(self, list1,list2):
-        dummy = ListNode(0, None)
-        current = dummy
-        headOne, headTwo = list1, list2
-        
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+        headOne, headTwo = l1, l2
         while headOne and headTwo:
             if headOne.val <= headTwo.val:
                 temp = headOne.next
-                current.next = headOne
-                current = current.next
+                curr.next = headOne
                 headOne = temp
             else:
                 temp = headTwo.next
-                current.next = headTwo
-                current = current.next
+                curr.next = headTwo
                 headTwo = temp
-                
+            curr = curr.next
         if headOne:
-            current.next = headOne
+            curr.next = headOne
         if headTwo:
-            current.next = headTwo
-        
+            curr.next = headTwo
         return dummy.next
+    
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
-        
+        res = []
         while len(lists) > 1:
             temp = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if i + 1 < len(lists) else None
-                temp.append(self.mergeLists(l1,l2))
-            lists = temp
-        
+                temp.append(self.mergeTwoLists(l1, l2))
+            lists = temp[:]
         return lists[0]
