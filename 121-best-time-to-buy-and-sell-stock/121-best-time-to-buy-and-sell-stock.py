4076e99b9f4c09ac3580e3944447257288f8c14d
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-09 20:23:09 -0700

    Time: 2692 ms (5.00%), Space: 25 MB (85.68%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,26 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-#         we cannot choose the max and min price and use them because the min could be before the max
+# we cannot choose the max and min price and use them because the min could be before the max
 # brute force : double for loops, check every subsequent day for every day and maintain a max profit so far - O(n**2) time space complexity
 # sliding window approach : right = 1 + left 
 # if rightVal - leftVal = -ve then update right and left pointers - right goes to where left was
 # if rightVal - leftVal = +ve then update right pointer and max_profit
 # O(n) time and O(1) space
-
-        n = len(prices)
-        if n < 2:
+        if len(prices) == 1:
             return 0
-        if n == 2 and prices[1] < prices[0]:
-            return 0
-        res = 0
         left, right = 0, 1
-        while right < n:
-            profit = prices[right] - prices[left]
-            if profit < 0:
+        res = 0
+        while right < len(prices):
+            if prices[left] > prices[right]:
                 left = right
-                right = left + 1 
+                right = left + 1
                 continue
-            
-            res = max(res, profit)
+            res = max(res, prices[right] - prices[left])
             right += 1
         return res
