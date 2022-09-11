edbc31a34192e23d413774724d5eb7382c1cffc0
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-11 16:13:09 -0700

    Time: 200 ms (7.58%), Space: 14.4 MB (80.18%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -2,12 +2,13 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
-        curMin,curMax = nums[0], nums[0]
-        res = nums[0]
+        
+        res = curMin = curMax = nums[0]
         for i in range(1, len(nums)):
             n = nums[i]
-            temp = n * curMin
-            curMin = min(n, temp, n * curMax)
-            curMax = max(n, temp, n * curMax)
+            temp = curMin
+            curMin = min(n, n * curMin, n * curMax)
+            curMax = max(n, n * temp, n * curMax)
             res = max(res, curMax)
+        
         return res
