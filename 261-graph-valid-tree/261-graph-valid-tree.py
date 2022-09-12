67b015bd8962f890951d7e73bd6ced75b6f2d8b1
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-12 10:42:00 -0700

    Time: 227 ms (7.34%), Space: 15.5 MB (74.23%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,6 +1,6 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        components = n 
+        components = n
         parent = [i for i in range(n)]
         rank = [1] * n
         
@@ -11,7 +11,7 @@ class Solution:
                 p = parent[p]
             return p
         
-        def union(n1,n2):
+        def union(n1, n2):
             p1, p2 = find(n1), find(n2)
             
             if p1 == p2:
@@ -24,11 +24,13 @@ class Solution:
                 parent[p1] = p2
                 rank[p2] += rank[p1]
             
-            return 1
-        for v1,v2 in edges:
-            if not union(v1,v2):
+            return True
+        
+        
+        for n1, n2 in edges:
+            if not union(n1, n2):
                 return False
-            else: 
+            else:
                 components -= 1
         
         return True if components == 1 else False
