4c5cc26789574fea5ff97c091ce234790192c460
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-12 10:04:27 -0700

    Time: 100 ms (55.99%), Space: 14.4 MB (46.87%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -3,23 +3,24 @@ class Solution:
         board = [["." for _ in range(n)] for _ in range(n)]
         col, pos, neg = set(), set(), set()
         res = []
-        def backtrack(r):
+        
+        def backtrack(i):
+            r = i
             if r == n:
                 res.append(["".join(row) for row in board])
+                return 
             for c in range(n):
                 if c in col or r + c in pos or r - c in neg:
                     continue
-                
                 
                 col.add(c)
                 pos.add(r+c)
                 neg.add(r-c)
                 board[r][c] = "Q"
-                backtrack(r+1)
+                backtrack(r + 1)
                 col.remove(c)
-                pos.remove(r+c)
-                neg.remove(r-c)
+                pos.remove(r + c)
+                neg.remove(r - c)
                 board[r][c] = "."
-                
         backtrack(0)
         return res
