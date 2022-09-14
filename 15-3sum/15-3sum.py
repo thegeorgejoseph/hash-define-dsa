4cff41f2e3aab756b38d7f5925482d2869921b3e
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-14 11:43:42 -0700

    Time: 1649 ms (42.27%), Space: 18.2 MB (39.14%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,20 +1,22 @@
 class Solution:
+    #for the love of god -- first check is "if" and second check is "while"
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
-        for idx, num in enumerate(nums):
-            if idx > 0 and num == nums[idx -1 ]:
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            left, right = idx + 1, len(nums) - 1
+            num = nums[i]
+            left, right = i + 1, len(nums) - 1
             while left < right:
                 total = num + nums[left] + nums[right]
-                if total > 0:
-                    right -= 1
-                elif total < 0:
+                if total < 0:
                     left += 1
+                elif total > 0:
+                    right -= 1
                 else:
                     res.append([num, nums[left], nums[right]])
                     left += 1
-                    while left < right and nums[left] == nums[left - 1]:
+                    while left < len(nums) and nums[left] == nums[left-1]:
                         left += 1
         return res
