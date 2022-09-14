8ddfff97e32a4df8b8916bccecd98d354121a6a8
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-14 15:27:07 -0700

    Time: 501 ms (18.41%), Space: 15.6 MB (26.00%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -4,13 +4,15 @@ class Solution:
 # find the middle element and then compare the target to the middle element
 # if the target is smaller than the middle element then we only need to search the first half
 # if the target is larger than the middle element then we need to search the second half
+        nums.sort()
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = left + ((right - left) // 2)
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
+            pivot = nums[mid]
+            if target > pivot:
                 left = mid + 1
-            else:
+            elif target < pivot:
                 right = mid - 1
+            else:
+                return mid
         return -1
