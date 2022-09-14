cba4f3ff59f39a77211701c026f520cc7f68e855
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-14 15:09:47 -0700

    Time: 361 ms (48.88%), Space: 17.5 MB (48.04%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -4,26 +4,26 @@ class Solution:
             return 0
         
         wordList.append(beginWord)
-        graph = collections.defaultdict(list)
+        graph = defaultdict(list)
+        
         for word in wordList:
             for j in range(len(word)):
-                pattern = word[:j] + "*" + word[j + 1:]
+                pattern = word[:j] + "*" + word[j+1:]
                 graph[pattern].append(word)
-            
+        
         queue = deque([beginWord])
         visit = set([beginWord])
-        res = 1
-        
+        seq = 1
         while queue:
-            for _ in range(len(queue)):
+            for i in range(len(queue)):
                 word = queue.popleft()
                 if word == endWord:
-                    return res
+                    return seq
                 for j in range(len(word)):
                     pattern = word[:j] + "*" + word[j + 1:]
-                    for neighbor in graph[pattern]:
-                        if neighbor not in visit:
-                            visit.add(neighbor)
-                            queue.append(neighbor)
-            res += 1
+                    for newWord in graph[pattern]:
+                        if newWord not in visit:
+                            visit.add(newWord)
+                            queue.append(newWord)
+            seq += 1
         return 0
