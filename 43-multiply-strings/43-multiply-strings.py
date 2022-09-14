a6c8217a704fbc08f4a93970e0e15b7eb9bdf4da
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-14 09:58:41 -0700

    Time: 252 ms (26.34%), Space: 14 MB (30.03%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,14 +1,13 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if "0" in [num1, num2]: return "0"
+        if "0" in [num1,  num2]: return "0"
         num1, num2 = num1[::-1], num2[::-1]
-        
         res = [0] * (len(num1) + len(num2))
         
         for i in range(len(num1)):
             for j in range(len(num2)):
                 temp = res[i + j] + (int(num1[i]) * int(num2[j]))
-                res[i + j + 1] += temp // 10
+                res[i + j + 1] += temp // 10 
                 res[i + j] = temp % 10
         
         res = res[::-1]
@@ -16,5 +15,4 @@ class Solution:
         while i < len(res) and res[i] == 0:
             i += 1
         
-                
         return "".join(map(str, res[i:]))
