4fc8ec14bd965b9dd992e0de465b1a018bbbff9a
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-16 08:21:28 -0700

    Time: 70 ms, Memory: 13.8 MB - LeetHub

---
+++ string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,34 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        # how to find convert a string number to integer? -> use ord('char') - ord('a') to get actual integer number
+        # how to create a number from left to right? -> use char * 10 + new char to get the number, try with 789 example. -> This handles leading 0s case automatically.
+        # how to strip leading and trailing spaces? -> use Python.strip()
+        # after the strip is done the resulting string could be empty so return 0
+        s = s.strip()
+        if not s: return 0
+        INT_MAX, INT_MIN = pow(2,31)-1, -pow(2,31)
+        res = 0
+        negative = False
+        if s[0] == "-":
+            negative = True
+        elif s[0] == "+":
+            negative = False
+        elif not s[0].isnumeric():
+            return 0
+        else:
+            res = res*10 + (ord(s[0])-ord("0"))
+        for i in range(1, len(s)):
+            if s[i].isnumeric():
+                res = res*10 + (ord(s[i])-ord("0"))
+                if negative and res > INT_MAX:
+                    return INT_MIN
+                elif res > INT_MAX:
+                    return INT_MAX
+            else:
+                break
+        if negative:
+            return -res
+        else:
+            return res
+        
+        
