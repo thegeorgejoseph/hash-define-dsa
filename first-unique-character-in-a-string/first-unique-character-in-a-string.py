cb48101c16be4aaa77885d583698ddf354d6ea0c
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-16 15:38:48 -0700

    Time: 245 ms, Memory: 14 MB - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,10 +1,9 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        cache = [0] * 26
+        cache = [0]* 26
         for char in s:
             cache[ord(char)-ord('a')] += 1
-        
-        for i,char in enumerate(s):
-            if cache[ord(char)-ord('a')] == 1:
+        for i, c in enumerate(s):
+            if cache[ord(c)-ord('a')] == 1:
                 return i
         return -1
