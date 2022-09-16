3f3114d7b976bae5e89fd62b511c1c908b7bb17c
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-15 20:27:56 -0700

    Time: 163 ms, Memory: 15.5 MB - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #hashmap method
         cache = {}
-        for i, num in enumerate(nums):
+        for i,num in enumerate(nums):
             if num in cache:
                 return [i, cache[num]]
-            cache[target-num] = i
+            cache[target-num] = i 
         return [-1,-1]
