d8cd90036103fcdb4ed35c64ec32117b3f00f489
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-17 15:38:25 -0700

    Time: 132 ms, Memory: 26.1 MB - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -11,16 +11,12 @@ class Solution:
         def dfs(node):
             if not node:
                 return None
-            
             if node == p or node == q:
                 return node
-            
             left = dfs(node.left)
             right = dfs(node.right)
-            
             if left and right:
                 return node
-            else:
-                return left or right
+            return left or right
         
         return dfs(root)
