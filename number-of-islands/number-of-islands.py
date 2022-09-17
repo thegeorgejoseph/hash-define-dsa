ed73ec3f5352fb6338c983cf4d1c9c5c59465899
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-17 16:01:34 -0700

    Time: 1094 ms, Memory: 21.8 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -4,18 +4,30 @@ class Solution:
         directions = [[1,0],[0,1],[-1,0],[0,-1]]
         visit = set()
         islands = 0
-        def dfs(r,c):
-            if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] != "1"):
-                return 
-            visit.add((r,c))
-            for dr, dc in directions:
-                row, col = r + dr, c + dc
-                dfs(row, col)
+        # def dfs(r,c):
+        #     if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] != "1"):
+        #         return 
+        #     visit.add((r,c))
+        #     for dr, dc in directions:
+        #         row, col = r + dr, c + dc
+        #         dfs(row, col)
+        def bfs(r,c):
+            queue = deque([(r,c)])
+            while queue:
+                r, c = queue.popleft()
+                if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] != "1"):
+                        continue
+                visit.add((r,c))
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    queue.append((row,col))
+                
         
         for r in range(ROWS):
             for c in range(COLS):
                 if (r,c) not in visit and grid[r][c] == "1":
-                    dfs(r,c)
+                    # dfs(r,c)
+                    bfs(r,c)
                     islands += 1
         
         return islands
