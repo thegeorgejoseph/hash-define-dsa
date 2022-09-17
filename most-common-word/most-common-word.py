2d83d1060682265893d9d1c1f2cd96a1f08e2093
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-16 17:08:18 -0700

    Time: 59 ms, Memory: 13.9 MB - LeetHub

--- most-common-word/most-common-word.py
+++ most-common-word/most-common-word.py
@@ -2,17 +2,15 @@ class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         normalised_string = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
         words = normalised_string.split()
-        banned_set = set(banned)
-        hashmap = defaultdict(int)
+        banned = set(banned)
+        freqs = {}
         
         for word in words:
-            if word not in banned_set:
-                hashmap[word] += 1
+            if word not in banned:
+                freqs[word] = 1 + freqs.get(word, 0)
         
-        max_value = max(hashmap.values())
+        max_val = max(freqs.values())
+        for key, val in freqs.items():
+            if val == max_val: return key
         
-        for key, value in hashmap.items():
-            if value == max_value:
-                return key
-            
         
