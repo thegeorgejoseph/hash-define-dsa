2200c82685cfa1df2aaa2cfb0f6f5ef3c18b9765
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-18 11:04:36 -0700

    Time: 334 ms, Memory: 18.1 MB - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -1,40 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals) == 1: return intervals
+        res = []
         intervals.sort(key = lambda x: x[0])
-        res = [intervals[0]]
-        for i in range(1,len(intervals)):
-            currentInterval = res[-1][:]
-            newInterval = intervals[i][:]
+        currentInterval = intervals[0]
+        for i in range(1, len(intervals)):
+            newInterval = intervals[i]
             if newInterval[0] > currentInterval[1]:
-                res.append(newInterval)
-                continue
+                res.append(currentInterval)
+                currentInterval = newInterval
             else:
-                res.pop()
-                res.append([min(currentInterval[0], newInterval[0]),max(currentInterval[1], newInterval[1])])
+                currentInterval = [min(currentInterval[0], newInterval[0]), max(currentInterval[1], newInterval[1])]
+        res.append(currentInterval)
         return res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         intervals.sort(key = lambda x: x[0])
-#         res = [intervals[0]]
-        
-#         for i in range(1, len(intervals)):
-#             newInterval = intervals[i]
-#             previousInterval = res[-1]
-            
-#             if newInterval[0] > previousInterval[1]:
-#                 res.append(newInterval)
-#             else:
-#                 previousInterval = [min(previousInterval[0],newInterval[0]), max(previousInterval[1], newInterval[1])]
-#                 res[-1] = previousInterval
-#         return res
