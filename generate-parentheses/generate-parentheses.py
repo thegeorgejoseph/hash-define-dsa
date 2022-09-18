03da9bfd4bd47d90684bed74a0991e9c3b7a240d
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-17 17:58:20 -0700

    Time: 70 ms, Memory: 14.3 MB - LeetHub

--- generate-parentheses/generate-parentheses.py
+++ generate-parentheses/generate-parentheses.py
@@ -1,19 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        def dfs(openBrackets, closedBrackets,s):
-            if openBrackets == closedBrackets == n:
-                res.append(s)
-                return 
-            if openBrackets < n:
-                s += "("
-                dfs(openBrackets + 1, closedBrackets,s)
-                s = s[:-1]
-            if closedBrackets < openBrackets:
-                s += ")"
-                dfs(openBrackets, closedBrackets + 1,s)
-                s = s[:-1]
+        def dfs(word, openBracket, closedBracket):
+            if openBracket == closedBracket == n:
+                res.append(word)
+                return
+            if openBracket < n:
+                dfs(word + "(", openBracket + 1, closedBracket)
+            if closedBracket < openBracket:
+                dfs(word + ")", openBracket, closedBracket + 1)
             
-        
-        dfs(0,0, "")
+        dfs("", 0, 0)
         return res
