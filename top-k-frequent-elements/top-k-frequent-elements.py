46e9fa8d9d297e572889326e70af0ae6acb332f5
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-18 13:30:56 -0700

    Time: 106 ms, Memory: 18.7 MB - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,14 +1,45 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         Counter = {}
+        for num in nums:
+            Counter[num] = 1 + Counter.get(num,0)
+        
+        maxHeap = []
+        for num, count in Counter.items():
+            heapq.heappush(maxHeap, (-count, num))
         res = []
-        for n in nums:
-            Counter[n] = 1 + Counter.get(n, 0)
-        freqs = [[] for i in range(len(nums) +1)]
-        for n, count in Counter.items():
-            freqs[count].append(n)
-        for i in range(len(freqs)-1,-1,-1):
-            for n in freqs[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        while maxHeap:
+            cnt, num = heapq.heappop(maxHeap)
+            res.append(num)
+            if len(res) == k:
+                return res
+        return res
+#         Counter = {}
+#         for num in nums:
+#             Counter[num] = 1 + Counter.get(num, 0)
+        
+#         temp = [[] for i in range(len(nums)+1)]
+#         for key, value in Counter.items():
+#             temp[value].append(key)
+#         res = []
+#         for i in range(len(temp)-1,-1,-1):
+#             if not temp[i]:
+#                 continue
+#             for n in temp[i]:
+#                 res.append(n)
+#                 if len(res) == k:
+#                     return res
+        
+#         Counter = {}
+#         for num in nums:
+#             Counter[num] = 1 + Counter.get(num, 0)
+        
+#         maxHeap = [(-value, key) for key, value in Counter.items()]
+#         heapq.heapify(maxHeap)
+#         res = []
+#         while maxHeap:
+#             value, ele = heapq.heappop(maxHeap)
+#             res.append(ele)
+#             if len(res) == k:
+#                 break
+#         return res
