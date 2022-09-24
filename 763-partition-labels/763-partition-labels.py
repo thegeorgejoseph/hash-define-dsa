8d32d7461493dc21b25e821aa66ba4fb4152e70c
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-23 17:50:29 -0700

    Time: 84 ms (25.80%), Space: 13.9 MB (69.05%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,17 +1,15 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        res = []
-        counts = {}
-        for i, c in enumerate(s):
-            counts[c] = i
-            
-        maxIdx = counts[s[0]]
-        left = 0
+        cache = {}
         for i in range(len(s)):
+            cache[s[i]] = i
+        maxIdx = cache[s[0]]
+        left = 0
+        res = []
+        for i in range(1, len(s)):
             if i > maxIdx:
-                res.append(maxIdx - left + 1 )
-                left = maxIdx + 1
-            maxIdx = max(maxIdx, counts[s[i]])
-        
+                res.append(maxIdx - left + 1)
+                left = i
+            maxIdx = max(maxIdx, cache[s[i]])
         res.append(maxIdx - left + 1)
         return res
