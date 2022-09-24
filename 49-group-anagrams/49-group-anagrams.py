310ce1ba7f35d49b13decdc16b9b62a92c1b6ab8
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-23 20:51:25 -0700

    Time: 313 ms (6.18%), Space: 19 MB (35.51%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,20 +2,8 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         cache = defaultdict(list)
         for word in strs:
-            key = sorted(word)
-            cache[tuple(key)].append(word)
+            temp = [0] * 26
+            for c in word:
+                temp[ord(c)-ord('a')] += 1
+            cache[tuple(temp)].append(word)
         return list(cache.values())
-#         cache =  defaultdict(list)
-#         for word in strs:
-#             temp = [0]*26
-#             for char in word:
-#                 temp[ord(char)-ord('a')] += 1
-#             cache[tuple(temp)].append(word)
-#         return list(cache.values())
-# #         sorted_strs = [sorted(tuple(w)) for w in strs]
-#         hashmap = defaultdict(list)
-        
-#         for i,w in enumerate(sorted_strs):
-#             hashmap[tuple(w)].append(strs[i])
-        
-#         return list(hashmap.values())
