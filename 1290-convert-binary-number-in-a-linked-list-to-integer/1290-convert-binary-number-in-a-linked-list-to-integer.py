8bd222821444ad3e61940a0e7f6729c8f323ba36
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-24 16:08:54 -0700

    Time: 36 ms (86.62%), Space: 13.8 MB (96.03%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        res = 0
+        curr = head
+        while curr:
+            res = res * 2 + curr.val
+            curr = curr.next
+        return res
