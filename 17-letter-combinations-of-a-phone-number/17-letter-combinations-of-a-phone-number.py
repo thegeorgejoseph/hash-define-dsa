b6179c7d7444dcbcfc9cb8bc9631b95a061a3bba
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-25 10:50:55 -0700

    Time: 62 ms (20.01%), Space: 14 MB (31.58%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,25 +1,19 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        cache = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
         res = []
-        mapping = {
-             "2": "abc",
-            "3": "def",
-            "4": "ghi",
-             "5": "jkl",
-             "6": "mno",
-             "7": "pqrs",
-             "8": "tuv",
-             "9": "wxyz"
-        }
-        
-        def dfs(i, string):
+        def dfs(i, word):
             if i == len(digits):
-                res.append(string)
+                res.append(word)
                 return 
-            for char in mapping[digits[i]]:
-                string += char
-                dfs(i + 1, string)
-                string = string[:-1]
             
-        if digits: dfs(0, "")
+            possible = cache[digits[i]]
+            for j in range(len(possible)):
+                word += possible[j]
+                dfs(i + 1, word)
+                word = word[:-1]
+            
+        dfs(0, "")
         return res
