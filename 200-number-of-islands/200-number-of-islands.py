90baf732e91946d029be48e3d5f58a459dce4054
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-26 21:39:30 -0700

    Time: 1026 ms (5.66%), Space: 25.1 MB (10.73%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,35 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        directions = [[0,1],[1,0],[0,-1],[-1,0]]
         visit = set()
-        
-        # def dfs(r,c):
-        #     if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] != "1"):
-        #         return 
-        #     visit.add((r,c))
-        #     for dr, dc in directions:
-        #         row, col = r + dr, c + dc
-        #         dfs(row, col)
-        def bfs(r,c):
-            queue = deque([(r,c)])
-            while queue:
-                r,c = queue.popleft()
-                if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] != "1"):
-                    continue
-                visit.add((r,c))
-                for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    queue.append((row, col))
-                    
+        directions = [[0,1],[1,0],[0,-1],[-1,0]]
+        islands = 0
+        def dfs(r,c):
             
-        islands = 0 
+            if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] != "1"):
+                return 
+            
+            visit.add((r,c))
+            for dr, dc in directions:
+                row, col = r + dr, c + dc
+                dfs(row, col)
+            
         for r in range(ROWS):
             for c in range(COLS):
                 if (r,c) not in visit and grid[r][c] == "1":
+                    dfs(r,c)
                     islands += 1
-                    # dfs(r,c)
-                    bfs(r,c)
+        
         return islands
-        
-        
