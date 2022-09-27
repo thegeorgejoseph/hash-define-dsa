98d9c0a28324176654b608a5cf468df5c7e78253
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-26 17:12:25 -0700

    Time: 37 ms (93.63%), Space: 13.9 MB (80.43%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -1,12 +1,16 @@
+# while stack is not empty and current character is lexicographically smaller than previous character and there is another item of the previous character
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
+        lastIdx = {c: i for i, c in enumerate(s)}
+        cache = set()
         stack = []
-        visit = set()
-        lastIdx = {c : i for i, c in enumerate(s)}
-        for i, char in enumerate(s):
-            if char not in visit:
-                while stack and char < stack[-1] and i < lastIdx[stack[-1]]:
-                    visit.remove(stack.pop())
-                visit.add(char)
-                stack.append(char)
+        for i, c in enumerate(s):
+            if c in cache:
+                continue
+            while stack and i < lastIdx[stack[-1]] and c < stack[-1]:
+                top = stack.pop()
+                cache.remove(top)          
+            else:
+                cache.add(c)
+                stack.append(c)
         return "".join(stack)
