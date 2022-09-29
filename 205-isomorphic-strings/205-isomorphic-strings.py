ccbaeed77496ded681d01920e8d4b3afe4fd1026
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-29 10:22:46 -0700

    Time: 77 ms (43.01%), Space: 14.2 MB (88.36%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,6 +1,7 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        map1, map2 = {}, {}
+        map1 = {}
+        map2 = {}
         for c1, c2 in zip(s,t):
             if c1 not in map1 and c2 not in map2:
                 map1[c1] = c2
