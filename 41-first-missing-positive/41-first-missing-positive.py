298f2a3f4f70ab79f9ebbfe76edea8e2c47a6243
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-09-30 15:01:18 -0700

    Time: 933 ms (33.25%), Space: 27.2 MB (66.11%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -11,9 +11,8 @@ class Solution:
                     nums[val - 1] *= -1
                 elif nums[val - 1] == 0:
                     nums[val - 1] = -1 * (len(nums) + 1)
-                    
+            
         for i in range(1, len(nums) + 1):
-            if nums[i - 1] >= 0:
-                return i
+            if nums[i - 1] >= 0: return i
         
-        return len(nums)  + 1
+        return len(nums) + 1
