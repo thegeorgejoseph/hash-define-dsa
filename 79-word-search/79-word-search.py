dc0b503369ea0960924836e8362d1ba86f212db7
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-01 10:06:37 -0700

    Time: 6161 ms (65.75%), Space: 14.1 MB (12.47%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,22 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
-        visit = set()
         directions = [[1,0],[0,1],[-1,0],[0,-1]]
-        def backtrack(r,c, i):
+        visit = set()
+        
+        def dfs(r,c, i):
             if i == len(word):
                 return True
-            if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or board[r][c] != word[i]):
+            if ( r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or board[r][c] != word[i]):
                 return False
+            
             visit.add((r,c))
             for dr, dc in directions:
                 row, col = r + dr, c + dc
-                if backtrack(row, col, i + 1): return True
+                if dfs(row, col, i + 1): return True
+            
             visit.remove((r,c))
             return False
-        
+            
+        i = 0
         for r in range(ROWS):
             for c in range(COLS):
-                if backtrack(r,c, 0): return True
-        
+                if board[r][c] == word[i]:
+                    if dfs(r,c, i): return True
+                
         return False
