fb2c1c9dea1ea03e7e055785eb750948dd91a550
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-02 22:10:54 -0700

    Time: 311 ms (32.09%), Space: 17.6 MB (31.82%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        directions = [[0,1],[1,0],[0,-1],[-1,0]]
         visit = set()
-        
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
+        maxArea = 0
         def dfs(r,c):
             if (r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] != 1):
                 return 0
@@ -13,11 +13,11 @@ class Solution:
                 row, col = r + dr, c + dc
                 res += dfs(row, col)
             return res
-            
-        res = 0
+        
+        
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 1:
-                    res = max(res, dfs(r,c))
-        
-        return res
+                if (r,c) not in visit and grid[r][c] == 1:
+                    maxArea = max(maxArea, dfs(r,c))
+                    
+        return maxArea
