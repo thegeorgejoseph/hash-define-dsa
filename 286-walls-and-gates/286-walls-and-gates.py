c0542f77c037961778574de63e0c6f7020b750ec
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-03 13:35:00 -0700

    Time: 225 ms (99.86%), Space: 17.2 MB (58.65%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -1,22 +1,23 @@
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         ROWS, COLS = len(rooms), len(rooms[0])
-        INF = 2147483647
-        queue = deque()
+        queue = deque() # we add the gates to the queue
         directions = [[1,0],[0,1],[-1,0],[0,-1]]
-        
+        EMPTY = pow(2,31)-1
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
                     queue.append((r,c))
-      
+        
         while queue:
-            for i in range(len(queue)):
+            for _ in range(len(queue)):
                 r, c = queue.popleft()
                 for dr, dc in directions:
                     row, col = r + dr, c + dc
-                    if (row < 0 or row == ROWS or col < 0 or col == COLS or rooms[row][col] != INF):
-                        continue 
+                    if (row < 0 or row == ROWS or col < 0 or col == COLS or rooms[row][col] != EMPTY):
+                        continue
                     rooms[row][col] = rooms[r][c] + 1
-                    queue.append((row,col))
+                    queue.append((row, col))
+        
+        
         
