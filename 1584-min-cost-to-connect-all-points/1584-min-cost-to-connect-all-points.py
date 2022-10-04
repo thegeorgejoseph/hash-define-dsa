cd5d73131ad193b164c7a817cc000a7c86503efc
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-04 12:56:18 -0700

    Time: 7303 ms (10.41%), Space: 172 MB (24.78%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,26 +1,24 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        graph = {i: [] for i in range(len(points))}
-        
+        graph = {i : [] for i in range(len(points))}
         for i in range(len(points)):
             x1, y1 = points[i]
-            for j in range(i, len(points)):
+            for j in range(i + 1, len(points)):
                 x2, y2 = points[j]
-                dist = abs(x2-x1) + abs(y2-y1)
+                dist = abs(x2 - x1) + abs(y2 - y1)
                 graph[i].append([dist, j])
-                graph[j].append([dist,i])
-        
-        minHeap = [[0,0]] #0 cost start from 0th point in points
+                graph[j].append([dist, i])
+        minHeap = [[0,0]]
         heapq.heapify(minHeap)
         visit = set()
-        res = 0
+        total = 0
         while minHeap and len(visit) < len(points):
             cost, node = heapq.heappop(minHeap)
             if node in visit: continue
+            total += cost
             visit.add(node)
-            res += cost
             
             for neiCost, nei in graph[node]:
-                heapq.heappush(minHeap,[neiCost, nei])
-        
-        return res if len(visit) == len(points) else 0
+                if nei not in visit:
+                    heapq.heappush(minHeap, [neiCost, nei])
+        return total if len(visit) == len(points) else 0
