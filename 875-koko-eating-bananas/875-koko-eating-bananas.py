9fb2580d9a7063bb081651943c2c7a1c9db47095
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-06 09:43:32 -0700

    Time: 1551 ms (9.01%), Space: 15.5 MB (74.47%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -3,21 +3,15 @@ class Solution:
         left, right = 1, max(piles)
         res = max(piles)
         while left <= right:
-            k = left + (right - left) // 2
+            k = left + ((right - left) //2)
             hours = 0
             for p in piles:
-                hours += math.ceil(p / k)
+                hours += math.ceil(p/k)
             if hours <= h:
+                res = min(res,k)
                 right = k - 1
-                res = min(res, k)
             else:
                 left = k + 1
-    
         return res
-    # [3,6,7,11]
-    # hours = 11 and then the guards return 
-    # clearly if she eats 11 bananas per hour then the number of hours is equal to the len(array)
-    # but what if k < len(hour) then it wont work i think
-    # 1 , 2, 3, 4, 5, 6, 7, 8,  9, 10, 11
-    # select the mid value and calculate the hours
-    # if the hours <= h then find a smaller value, else find a larger value
+                
+        
