1e295daefe421472ca419daf9a16387f4b3901c2
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-07 10:14:45 -0700

    Time: 223 ms (25.42%), Space: 13.9 MB (34.79%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,28 +1,14 @@
-from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         ROWS, COLS = len(board), len(board[0])
-        valid = "123456789"
-        row_set = defaultdict(set)
-        col_set = defaultdict(set)
-        quad_set = defaultdict(set)
-        for i in range(ROWS):
-            for j in range(COLS):
-                if board[i][j] == ".":
-                    continue
-                if (board[i][j] not in valid or board[i][j] in row_set[i] or
-                    board[i][j] in col_set[j] or board[i][j] in quad_set[(i // 3, j // 3)]):
+        row_set, col_set, square = defaultdict(set), defaultdict(set), defaultdict(set)
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == ".": continue
+                if board[r][c] in row_set[r] or board[r][c] in col_set[c] or board[r][c] in square[(r//3,c//3)]:
                     return False
-                row_set[i].add(board[i][j])
-                col_set[j].add(board[i][j])
-                quad_set[(i // 3, j // 3)].add(board[i][j])
+                row_set[r].add(board[r][c])
+                col_set[c].add(board[r][c])
+                square[(r//3,c//3)].add(board[r][c])
         return True
-    
-    
-    
-# we need a set to keep track of each row and column - map of sets? - defaultdict(set)
-# we need a set to keep track of the divided part of the board - (i // 3, j // 3)
-# double for loop and create the sets 
-#  anytime something is not "." check
-# if it is in between 1-9, and if it is not not there in the sets then add it to the sets, but if it is there in the sets return False
-# if it works return True
