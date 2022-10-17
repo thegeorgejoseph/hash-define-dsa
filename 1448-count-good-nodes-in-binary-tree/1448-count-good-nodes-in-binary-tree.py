ca3eb6bb1a6f7a5776ae97fdffe81ba99f79cfe1
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-17 10:13:32 -0700

    Time: 635 ms (18.73%), Space: 32.7 MB (8.40%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -11,7 +11,7 @@ class Solution:
             if not node:
                 return 0
             
-            left  = dfs(node.left, max(maxSoFar, node.val))
+            left = dfs(node.left, max(maxSoFar, node.val))
             right = dfs(node.right, max(maxSoFar, node.val))
             
             if maxSoFar > node.val:
@@ -19,28 +19,7 @@ class Solution:
             else:
                 return left + right + 1
             
-        
         return dfs(root, root.val)
-            
-        #solution without extra space
-        # def dfs(node, max_so_far):
-        #     if not node:
-        #         return 0
-        #     left = dfs(node.left, max(max_so_far, node.val))
-        #     right = dfs(node.right, max(max_so_far, node.val))
-        #     res = left + right
-        #     res += 1 if max_so_far <= node.val else 0
-        #     return res
-        # return dfs(root, root.val)
-        #This is using extra memory even though the time complexity is not affected, lets try passing the solution up
-#         count = [0]
-#         def dfs(node, max_so_far,count):
-#             if not node:
-#                 return
-#             left = dfs(node.left, max(max_so_far, node.val),count)
-#             right = dfs(node.right, max(max_so_far, node.val),count)
-            
-#             if max_so_far <= node.val:
-#                 count[0] += 1
-#         dfs(root, root.val,count)
-#         return count[0]
+        
+        
+        
