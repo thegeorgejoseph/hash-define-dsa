4268a4c17b27cd8f8c609e06d0667c71b8f5285a
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-19 22:21:56 -0700

    Time: 587 ms (90.04%), Space: 36.2 MB (31.50%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -6,20 +6,20 @@ class MedianFinder:
     def addNum(self, num: int) -> None:
         heapq.heappush(self.lower, -num)
         
-        if self.lower and self.upper and -1 * self.lower[0] > self.upper[0]:
-            top = -1* heapq.heappop(self.lower)
-            heapq.heappush(self.upper, top)
-        if len(self.lower) -  len(self.upper) > 1:
-            top = -1* heapq.heappop(self.lower)
-            heapq.heappush(self.upper, top)
-        elif len(self.upper) - len(self.lower) > 1:
-            top = heapq.heappop(self.upper)
-            heapq.heappush(self.lower, -top)
+        if self.lower and self.upper and -1* self.lower[0] > self.upper[0]:
+            num = -1 * heapq.heappop(self.lower)
+            heapq.heappush(self.upper, num)
         
+        if len(self.lower) > 1 + len(self.upper):
+            num = -1 * heapq.heappop(self.lower)
+            heapq.heappush(self.upper, num)
+        elif len(self.upper) > 1 + len(self.lower):
+            num = heapq.heappop(self.upper)
+            heapq.heappush(self.lower, -num)
 
     def findMedian(self) -> float:
         if len(self.lower) == len(self.upper):
-            return (-self.lower[0] + self.upper[0]) / 2
+            return ((-self.lower[0] + self.upper[0]) / 2)
         if len(self.lower) > len(self.upper):
             return -self.lower[0]
         else:
