dadff99ac9c5afb0b3855f85af193f556e6a7dd1
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-22 19:11:55 -0700

    Time: 284 ms (13.74%), Space: 19.3 MB (29.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,19 @@
-# heap solution - O(N) / O(N) space
+# heap solution - O(N) / O(N) spac
+# bucket sort method - 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        buckets = [[] for _ in range(len(nums)+1)]
         cache = {}
         for n in nums:
-            cache[n] = cache.get(n, 0) + 1
-        maxHeap = []
+            cache[n] = cache.get(n, 0 ) + 1
         for el, freq in cache.items():
-            heapq.heappush(maxHeap, (-freq, el))
-        res = []
-        while maxHeap:
-            _, el = heapq.heappop(maxHeap)
-            res.append(el)
-            if len(res) == k:
-                return res
+            buckets[freq].append(el)
+        for i in range(len(buckets)-1, -1, -1):
+            for el in buckets[i]:
+                res.append(el)
+                if len(res) == k:
+                    return res
         return res
-        
+                
+            
