d89110e7c3d0785648283c0943837fec6249259d
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-22 18:57:53 -0700

    Time: 118 ms (86.73%), Space: 19 MB (36.49%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        cache = defaultdict(list)
+        for word in strs:
+            temp = [0]*26
+            for c in word:
+                temp[ord(c)-ord('a')] += 1
+            cache[tuple(temp)].append(word)
+        return list(cache.values())
