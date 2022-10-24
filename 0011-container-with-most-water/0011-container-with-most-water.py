0e4eb6d98c77d38e3aee6467cfa9ab349d18ea47
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-23 19:55:03 -0700

    Time: 1866 ms (32.54%), Space: 26.1 MB (91.76%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,10 +3,10 @@ class Solution:
         res = 0
         left, right = 0, len(height) - 1
         while left < right:
-            res = max(res, (right - left)*min(height[left], height[right]))
-            if height[left] < height[right]:
+            trapped = min(height[left], height[right])*(right-left)
+            res = max(res, trapped)
+            if height[left] <= height[right]:
                 left += 1
             else:
                 right -= 1
         return res
-            
