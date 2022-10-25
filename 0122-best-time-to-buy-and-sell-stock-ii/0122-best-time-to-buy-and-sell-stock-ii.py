e1a3435c024da282fa2c3afa64d3d94a8ae81995
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-25 16:02:23 -0700

    Time: 144 ms (30.05%), Space: 15 MB (99.76%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,12 +1,24 @@
+'''
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+
+'''
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
+        left, right = 0, 0
         res = 0
-        while r < len(prices):
-            profit = prices[r] - prices[l]
-            if profit > 0:
+        while left < len(prices) and right < len(prices):
+            profit = prices[right] - prices[left]
+            if profit < 0:
+                left = right
+                right = right
+            else:
                 res += profit
-            l += 1
-            r += 1     
+                left = right
+                right = right
+            right += 1
         return res
-            
