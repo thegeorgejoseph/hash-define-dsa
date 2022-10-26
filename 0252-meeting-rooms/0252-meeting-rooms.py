a55f5c2ff26c24ee1ba5f6ed19ffdb0a8c8961f8
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-25 17:34:35 -0700

    Time: 91 ms (82.81%), Space: 17.2 MB (98.44%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if not intervals:
+            return True
+        intervals.sort()
+        currentInterval = intervals[0]
+        for i in range(1, len(intervals)):
+            newInterval = intervals[i]
+            if newInterval[0] >= currentInterval[1]:
+                currentInterval = [min(currentInterval[0], newInterval[0]), max(currentInterval[1], newInterval[1])]
+            else:
+                return False
+        return True
