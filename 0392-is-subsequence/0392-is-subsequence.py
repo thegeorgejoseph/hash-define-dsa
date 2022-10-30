a787f5906dee66c083fcbd42727f4e9c95b2ed56
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-10-30 16:21:08 -0700

    Time: 62 ms (48.33%), Space: 13.9 MB (41.98%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,4 +1,9 @@
 '''
+
+Two Pointers Solution
+
+
+DP Levenshtein Distance Solution
     a  h  b  g  d  c  " "
 a   T                  F
 b         T        T   
@@ -6,11 +11,7 @@ c   5  4  3  2  1  0   1
 
 Subsequence is like Levenshtein Distance with deletions, if number of deletions required is less than length of target
 
-
-'''
-class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        dp = [[0 for j in range(len(t) + 1)] for i in range(len(s) + 1)]
+dp = [[0 for j in range(len(t) + 1)] for i in range(len(s) + 1)]
         dp[-1][-1] = 0
         for i in range(len(s)):
             dp[i][len(t)] = len(s) - i
@@ -24,3 +25,20 @@ class Solution:
                     dp[i][j] = min(dp[i][j + 1], dp[i + 1][j]) + 1
         
         return True if (len(t) - dp[0][0] >= len(s)) else False
+
+'''
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if s == "":
+            return True
+        if t == "":
+            return False
+        s_ptr, t_ptr = 0,0
+        while t_ptr < len(t):
+            if s[s_ptr] == t[t_ptr]:
+                s_ptr += 1
+                if s_ptr == len(s):
+                    return True
+            t_ptr += 1
+        return False
+                
