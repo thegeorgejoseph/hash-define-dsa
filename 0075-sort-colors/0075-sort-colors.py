46614ade5d1b9aad2a6296102a329dc4eb3c0fb3
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-03 21:29:34 -0700

    Time: 62 ms (56.47%), Space: 13.8 MB (97.30%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -6,6 +6,16 @@ Counting Sort Technique - three variables to hold the counts of each value
 Iterate over the array and replace accordingly
 Two Pass
 
+cache = {0 : 0, 1: 0, 2: 0}
+        for num in nums:
+            cache[num] += 1
+        idx = 0
+        for key, value in cache.items():
+            for i in range(value):
+                nums[idx] = key
+                idx += 1
+        
+        
 Quick Sort Partition Method 
 Left Ptr holds the 0 boundary
 Right Ptr holds the 2 boundary
@@ -18,16 +28,19 @@ do not increment i if right is swapped because there could be a 1 on the left
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        cache = {0 : 0, 1: 0, 2: 0}
-        for num in nums:
-            cache[num] += 1
-        idx = 0
-        for key, value in cache.items():
-            for i in range(value):
-                nums[idx] = key
-                idx += 1
+        left, right = 0, len(nums)-1
+        i = 0
+        while i <= right:
+            if nums[i] == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[right], nums[i] = nums[i], nums[right]
+                right -= 1
+            else:
+                i += 1
+        
+        
         
         
