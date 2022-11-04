7d1ecb4718b2df3a45e8fe21258890590722daca
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-04 16:59:51 -0700

    Time: 196 ms (37.79%), Space: 14.4 MB (38.72%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,29 +1,20 @@
 '''
-Input: nums = [2,3,-2,4]
+Input: nums = [2,3,-2,4, -1]
 Output: 6
-Explanation: [2,3] has the largest product 6.
 
- **contiguous non-empty subarray
- **return the product.
+curMin = 2 ,3, -12, -48, -4
+curMax = 2, 6, -2, 4, 48
 
-Input: nums = [-2,0,-1]
-Output: 0
-Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
-
-[-2,1,-4] -> 8
-with our logic we will return 1
-
-curMin = [-2,-2,-4] -> use temp not to update curMin preemptively
-curMax = [-2,1,8]  -> result comes from here
-min-max(num*curMin, num*curMax, num)
+[3,-2,4]
+res = 6
+use temp to store curMin
 '''
-
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        curMin, curMax, res = nums[0], nums[0], nums[0]
-        for i in range(1,len(nums)):
+        res, curMin, curMax = nums[0], nums[0], nums[0]
+        for num in nums[1:]:
             temp = curMin
-            curMin = min(curMin*nums[i], curMax*nums[i], nums[i])
-            curMax = max(temp*nums[i], curMax*nums[i], nums[i])
+            curMin = min(num, num * curMin, num * curMax)
+            curMax = max(num, num * temp, num * curMax)
             res = max(res, curMax)
         return res
