5dae53961466f9a24f8cdcf255695ca760b6af9f
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-07 13:54:32 -0800

    Time: 154 ms (66.94%), Space: 17.6 MB (50.75%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,15 +1,23 @@
+
+'''
+Input: intervals = [[0,30],[5,10],[15,20]]
+starts = [0,5,15]
+ends = [10,20,30]
+whenever a meeting starts before a meeting ends rooms += 1
+whenever a meeting ends, rooms -= 1
+update global res
+'''
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        starts = sorted([interval[0] for interval in intervals])
-        ends = sorted([interval[1] for interval in intervals])
-        res, rooms = 0, 0
-        left, right = 0, 0
-        while left < len(intervals) and right < len(intervals):
-            if starts[left] < ends[right]:
+        res, i, j, rooms = 0, 0, 0, 0
+        starts = sorted([vals[0] for vals in intervals])
+        ends = sorted([vals[1] for vals in intervals])
+        while i < len(intervals) and j < len(intervals):
+            if starts[i] < ends[j]:
                 rooms += 1
-                left += 1
+                i += 1
             else:
                 rooms -= 1
-                right += 1
+                j += 1
             res = max(res, rooms)
         return res
