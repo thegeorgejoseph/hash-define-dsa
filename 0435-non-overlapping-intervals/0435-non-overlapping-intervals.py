d963c97e2f38bf62544e5934f15f95b41a0368fe
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-07 13:33:47 -0800

    Time: 3412 ms (34.06%), Space: 53 MB (23.17%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,13 +1,26 @@
+'''
+Input: intervals = [[1,3], [1,2],[2,3],[3,4]]
+
+intervals.sort()
+1,2,3
+1,2
+remove the largest each time there is a merge conflict so that we minimise the future conflicts to be removed
+curInterval = intervals[0]
+newInterval = 
+
+'''
+
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
+        intervals.sort(key = lambda x: x[0])
+        curInterval = intervals[0]
         res = 0
-        currentInterval = intervals[0]
-        for i in range(1, len(intervals)):
-            newInterval = intervals[i]
-            if newInterval[0] >= currentInterval[1]:
-                currentInterval = newInterval[:]
+        for i, newInterval in enumerate(intervals[1:]):
+            if newInterval[0] >= curInterval[1]:
+                curInterval = newInterval[:]
             else:
                 res += 1
-                currentInterval = [min(currentInterval[0], newInterval[0]), min(currentInterval[1], newInterval[1])]
+                curInterval = [min(curInterval[0], newInterval[0]), min(curInterval[1], newInterval[1])]
         return res
+            
+        
