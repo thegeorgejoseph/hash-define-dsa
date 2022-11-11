38ca248e2cd6c095176a8536a21455b6333cd1ca
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-11 00:37:05 -0800

    Time: 260 ms (28.45%), Space: 18.9 MB (32.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         cache = defaultdict(list)
-        for word in strs:
-            temp = [0]*26
-            for c in word:
-                temp[ord(c)-ord('a')] += 1
-            cache[tuple(temp)].append(word)
+        for w in strs:
+            pos = [0]*26
+            for c in w:
+                pos[ord(c)-ord('a')] += 1
+            cache[tuple(pos)].append(w)
         return list(cache.values())
+            
