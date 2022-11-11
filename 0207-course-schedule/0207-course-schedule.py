de7a2e0e1664851322975593e9af0837c32d0e6e
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-11 00:04:58 -0800

    Time: 127 ms (81.60%), Space: 17.1 MB (25.21%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,22 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = {i : [] for i in range(numCourses)}
+        graph = {i: [] for i in range(numCourses)}
+        
         for crs, pre in prerequisites:
             graph[crs].append(pre)
+        
         cycle = set()
         
         def dfs(course):
-            if course in cycle: return False
-            if not graph[course]: return True
+            if course in cycle: 
+                return False
+            if not graph[course]:
+                return True
             
             cycle.add(course)
             for pre in graph[course]:
-                if not dfs(pre): return False
+                if not dfs(pre):
+                    return False
             cycle.remove(course)
             graph[course] = []
             return True
+            
+            
         
         for crs, _ in graph.items():
             if not dfs(crs): return False
         
-        return True
+        return True 
