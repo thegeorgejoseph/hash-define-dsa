a4e29a506f859714bc921239669931289d92ceb5
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-11 00:34:49 -0800

    Time: 103 ms (89.82%), Space: 13.9 MB (35.35%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,13 +1,15 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows, cols, nine = defaultdict(set), defaultdict(set), defaultdict(set)
         ROWS, COLS = len(board), len(board[0])
+        rows, cols, sub = defaultdict(set), defaultdict(set), defaultdict(set)
+        
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == ".": continue
-                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in nine[(r//3, c //3)]:
+                if board[r][c] == ".":
+                    continue
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in sub[(r//3, c//3)]: 
                     return False
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
-                nine[(r//3,c//3)].add(board[r][c])
+                sub[(r//3, c//3)].add(board[r][c])
         return True
