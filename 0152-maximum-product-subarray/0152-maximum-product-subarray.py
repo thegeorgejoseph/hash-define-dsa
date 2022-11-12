7f2de6334f17b7a5b825cf87f53c83c63b887e98
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-11 18:57:14 -0800

    Time: 84 ms (96.80%), Space: 14.4 MB (38.21%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,20 +1,12 @@
-'''
-Input: nums = [2,3,-2,4, -1]
-Output: 6
-
-curMin = 2 ,3, -12, -48, -4
-curMax = 2, 6, -2, 4, 48
-
-[3,-2,4]
-res = 6
-use temp to store curMin
-'''
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        res, curMin, curMax = nums[0], nums[0], nums[0]
-        for num in nums[1:]:
+        res = nums[0]
+        curMax, curMin = nums[0], nums[0]
+        
+        for i in range(1, len(nums)):
             temp = curMin
-            curMin = min(num, num * curMin, num * curMax)
-            curMax = max(num, num * temp, num * curMax)
+            curMin = min(nums[i], curMin*nums[i], curMax*nums[i])
+            curMax = max(nums[i], temp*nums[i], curMax*nums[i])
             res = max(res, curMax)
+        
         return res
