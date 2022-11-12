6ebb8c4bcbd01076014c59d60561eee2c3fbec5d
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-12 14:39:02 -0800

    Time: 77 ms (56.86%), Space: 14 MB (12.64%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,13 +1,13 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         left, right = 1, n
+        res = 0
         while left <= right:
-            k = left + ((right-left)//2)
-            total = (k * (k + 1)) // 2
-            if total == n:
-                return k
+            mid = left + ((right-left)//2)
+            total = mid * (mid + 1) // 2
             if total > n:
-                right = k - 1
+                right = mid - 1
             else:
-                left = k + 1
-        return right
+                left = mid + 1
+                res = max(res, mid)
+        return res
