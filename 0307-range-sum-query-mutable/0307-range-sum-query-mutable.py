588fa409f6890c82b76f60a224d143cea535735b
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-13 16:11:53 -0800

    Time: 4261 ms (66.20%), Space: 49.3 MB (19.39%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,17 +1,16 @@
 class SegmentTree:
-    def __init__(self,total, L, R):
-        self.left = None
-        self.right = None
-        self.L = L
-        self.R = R
-        self.sum = total
     
+    def __init__(self, total, L, R):
+        self.sum = total
+        self.L, self.R = L, R
+        self.left, self.right = None, None
+        
     @staticmethod
     def build(nums, L, R):
         if L == R:
             return SegmentTree(nums[L], L, R)
         
-        M = (L + R) // 2
+        M = (L + R ) // 2
         root = SegmentTree(0, L, R)
         root.left = SegmentTree.build(nums, L, M)
         root.right = SegmentTree.build(nums, M + 1, R)
@@ -21,13 +20,13 @@ class SegmentTree:
     def update(self, index, val):
         if self.L == index and self.R == index:
             self.sum = val
-            return
+            return 
+        
         M = (self.L + self.R) // 2
         if index > M:
             self.right.update(index, val)
         else:
             self.left.update(index, val)
-        
         self.sum = self.left.sum + self.right.sum
     
     def rangeQuery(self, L, R):
@@ -41,12 +40,11 @@ class SegmentTree:
             return self.left.rangeQuery(L, R)
         else:
             return (self.left.rangeQuery(L, M) + self.right.rangeQuery(M + 1, R))
-        
     
 class NumArray:
-    
+
     def __init__(self, nums: List[int]):
-        self.root = SegmentTree.build(nums, 0, len(nums) - 1)
+        self.root = SegmentTree.build(nums, 0, len(nums)-1)
 
     def update(self, index: int, val: int) -> None:
         self.root.update(index, val)
