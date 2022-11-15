d51a3f19dd25e13e1907a92971bdafd19b992dcf
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-15 13:54:58 -0800

    Time: 204 ms (31.77%), Space: 15.4 MB (50.46%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,26 +1,28 @@
 class Solution:
+
+    def binarySearch(self, nums, first, last, leftSearch, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + ((right-left)//2)
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                if leftSearch:    
+                    first = min(first, mid)
+                    right = mid - 1
+                else:
+                    last = max(last, mid)
+                    left = mid + 1
+        if leftSearch:
+            return first
+        else:
+            return last
+        
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         first, last = float('inf'), float('-inf')
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = left + ((right-left)//2)
-            if nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                first = min(first, mid)
-                right = mid - 1
-                
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = left + ((right-left)//2)
-            if nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                last = max(last, mid)
-                left = mid + 1
+        first = self.binarySearch(nums, first, last, True, target)
+        last = self.binarySearch(nums, first, last, False, target)
         if first == float('inf') and last == float('-inf'): return [-1,-1]
         return [first, last]
