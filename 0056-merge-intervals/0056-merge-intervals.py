0c5e675c37294ccc36561e6cb07694d0d00ec19e
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-21 11:22:00 -0800

    Time: 401 ms (15.45%), Space: 18 MB (85.72%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,14 +1,3 @@
-'''
-Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-res = [[1,3]]
-curInterval = res[-1]
-newInterval = [2,6]
-pop, Merge, append
-Intervals.sort()
-
-
-'''
-
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key = lambda x: x[0])
@@ -18,7 +7,9 @@ class Solution:
             if newInterval[0] > curInterval[1]:
                 res.append(newInterval)
             else:
-                prev = res.pop()
-                curInterval = [min(prev[0], newInterval[0]), max(prev[1], newInterval[1])]
+                curInterval = res.pop()
+                curInterval = [min(curInterval[0], newInterval[0]), max(curInterval[1], newInterval[1])]
                 res.append(curInterval)
+        
         return res
+        
