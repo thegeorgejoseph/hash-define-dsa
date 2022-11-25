fa39a779cc7458e6296b03d31c0c2516dd9eb4a0
George Joseph <37734939+thegeorgejoseph@users.noreply.github.com>
2022-11-24 16:56:14 -0800

    Time: 2410 ms (13.57%), Space: 75.9 MB (37.11%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,45 +1,50 @@
 class ListNode:
-    def __init__(self, key = 0, val = 0, left = None, right = None):
+    def __init__(self, key, val, left=None, right=None):
         self.key = key
         self.val = val
-        self.left = left
-        self.right = right
-
+        self.left, self.right = None, None
+        
 class LRUCache:
-    def __init__(self, capacity: int):
-        self.cache = {} # key - > key, value
-        self.cap = capacity
-        self.least, self.most = ListNode(), ListNode()
-        self.least.right, self.most.left = self.most, self.least
 
-    def get(self, key: int) -> int:
-        if key not in self.cache: return -1
-        self.remove(self.cache[key])
-        self.insert(self.cache[key])
-        return self.cache[key].val
-    
-    def remove(self, node):
-        PRV, NXT = node.left, node.right
-        PRV.right, NXT.left = NXT, PRV
-    
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.cap = capacity
+        self.least, self.most = ListNode(0,0), ListNode(0,0)
+        self.least.right, self.most.left = self.most, self.least
+        
     def insert(self, node):
         mru = self.most.left
         mru.right = node
         node.left = mru
         node.right = self.most
         self.most.left = node
+    
+    def remove(self, node):
+        PREV, NXT = node.left, node.right
+        PREV.right, NXT.left = NXT, PREV
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        else:
+            return -1
         
+        self.insert(self.cache[key])
+        return self.cache[key].val
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
+        
         self.cache[key] = ListNode(key, value)
         self.insert(self.cache[key])
+        
         if len(self.cache) > self.cap:
             lru = self.least.right
             self.remove(lru)
             del self.cache[lru.key]
             
-
+        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
